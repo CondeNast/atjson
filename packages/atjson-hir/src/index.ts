b37c6fe5acbffef8c0fd6dd9f86aa5a537defa83
Blaine Cook <blaine_cook@condenast.com>
2017-09-08 16:10:08 +0100

    move markdown renderer to separate package

--- packages/atjson-hir/src/index.ts
+++ packages/atjson-hir/src/index.ts
@@ -1,5 +1,4 @@
 import { Annotation, AtJSON } from './interfaces';
 import HIRNode from './hir-node';
 import HIR from './hir';
-import ReactRenderer from './react-renderer';
-export { Annotation, AtJSON, HIRNode, HIR, ReactRenderer };
+export { Annotation, AtJSON, HIRNode, HIR };
