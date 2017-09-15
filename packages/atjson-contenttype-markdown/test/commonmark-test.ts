3f7f3fe3bb165b430e0171337d5f8653b23d69c6
Blaine Cook <blaine_cook@condenast.com>
2017-09-15 15:03:26 +0100

    build out commonmark test; uses entire commonmark test suite (except html, currently disabled)

--- packages/atjson-contenttype-markdown/test/commonmark-test.ts
+++ packages/atjson-contenttype-markdown/test/commonmark-test.ts
@@ -1,29 +1,53 @@
 import { Parser } from 'atjson-contenttype-markdown';
+import { Parser as HTMLParser } from 'atjson-contenttype-html';
+import { HIR } from 'atjson-hir';
+import { AtJSON } from 'atjson';
 
 if (QUnit.isLocal) {
   const spec = require('commonmark-spec');
 
   const testModules = spec.tests.reduce((modules, test) => {
     if (!modules[test.section]) modules[test.section] = [];
-    console.log('test section', test.section, test);
     modules[test.section].push(test);  
     return modules;
-  }, {})
+  }, {});
 
   Object.keys(testModules).forEach((module) => {
-    console.log('module', module, testModules[module]);
+
+    if (module.match(/html/i)) return;
     const moduleTests = testModules[module];
 
     QUnit.module(module);
     
     moduleTests.forEach(test => {
-      QUnit.test(test.number, () => {
+      QUnit.test("\n\n--- markdown --->" + test.markdown + "<---\n--- html --->" + test.html + "<---\n\n", assert => {
+
+        test.markdown = test.markdown.replace(/→/g, '\t');
+        test.html = test.html.replace(/→/g, '\t');
+
         let parser = new Parser(test.markdown);
-        QUnit.assert.ok(parser.parse());
-        QUnit.assert.equal(parser.parse(), test.html);
+        let htmlParser = new HTMLParser(test.html);
+
+        let parsedMarkdown = parser.parse();
+        let parsedHtml = htmlParser.parse();
+
+        let mdAtJSON = new AtJSON({
+          content: parsedMarkdown.content,
+          contentType: 'text/commonmark',
+          annotations: parsedMarkdown.annotations
+        });
+
+        let htmlAtJSON = new AtJSON({
+          content: test.html,
+          contentType: 'text/html',
+          annotations: parsedHtml
+        });
+
+        let markdownHIR = new HIR(mdAtJSON).toJSON();
+        let htmlHIR = new HIR(htmlAtJSON).toJSON();
+
+        assert.deepEqual(JSON.stringify(markdownHIR), JSON.stringify(htmlHIR));
       });
     });
   });
 }
-
-export QUnit;
