eee39023b5fbc41ddbef41584820c49cd6a8ab6f
Tim Evans <tim_evans@condenast.com>
2017-09-19 17:51:36 -0400

    get the TextRenderer working with a simple document & with virtual annotations

--- packages/atjson-text-renderer/src/index.ts
+++ packages/atjson-text-renderer/src/index.ts
@@ -2,7 +2,7 @@ import Renderer from 'atjson-renderer';
 
 export default class TextRenderer extends Renderer {
   *renderAnnotation() {
-    let text = yield;
+    let text: string[] = yield;
     return text.join('');
   }
 }
--- packages/atjson-text-renderer/test/atjson-text-renderer-test.ts
+++
@@ -1,7 +0,0 @@
-import hello from 'atjson-text-renderer';
-
-QUnit.module('atjson-text-renderer tests');
-
-QUnit.test('hello', assert => {
-  assert.equal(hello(), 'Hello from atjson-text-renderer');
-});
