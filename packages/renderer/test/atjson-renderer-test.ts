000efb851f817e8cb0db1b191d0fbcc8e00316e4
Tim Evans <tim_evans@condenast.com>
2017-09-28 17:46:27 -0400

    chore: add real tests for @atjson/renderer

--- packages/renderer/test/atjson-renderer-test.ts
+++ packages/renderer/test/atjson-renderer-test.ts
@@ -1,8 +1,51 @@
+import { AtJSON } from '@atjson/core';
+import { HIR } from '@atjson/hir';
 import Renderer from '@atjson/renderer';
 
 QUnit.module('@atjson/renderer tests');
 
-QUnit.test('hello', assert => {
+QUnit.test('renderAnnotation is required on the base class', function (assert) {
   let renderer = new Renderer();
-  assert.equal(renderer.render(), 'Hello from @atjson/renderer');
+  assert.throws(function () {
+    renderer.render(new HIR(new AtJSON('Hello')));
+  });
+});
+
+QUnit.test('renderer abstract interface', function (assert) {
+  let hir = new HIR(new AtJSON({
+    content: 'This is bold and italic text',
+    annotations: [{
+      type: 'bold', start: 8, end: 17
+    }, {
+      type: 'italic', start: 12, end: 23
+    }]
+  }));
+
+  let root = hir.toJSON();
+  let callStack = [
+    root,
+    root.children[1],
+    root.children[1].children[1],
+    root.children[2]
+  ];
+
+  let textBuilder = [
+    ' and ',
+    'bold and ',
+    'italic',
+    'This is bold and italic text'
+  ];
+
+  class ConcreteRenderer extends Renderer {
+    *renderAnnotation(annotation) {
+      assert.deepEqual(annotation, callStack.shift());
+
+      let text: string[] = yield;
+      assert.deepEqual(text.join(''), textBuilder.shift());
+      return text.join('');
+    }
+  }
+
+  let renderer = new ConcreteRenderer();
+  renderer.render(hir);
 });
