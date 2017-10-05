024a7ec3dfcee8712edbeacefa9f3a36768f099b
Blaine Cook <blaine_cook@condenast.com>
2017-10-05 03:59:22 +0100

    as-yet unfixed test for incorrect behaviour of html contenttype and hir

--- packages/contenttype-html/test/atjson-contenttype-html-test.ts
+++ packages/contenttype-html/test/atjson-contenttype-html-test.ts
@@ -168,4 +168,31 @@ describe('@atjson/contenttype-html', function () {
       }]
     });
   });
+
+  it('<p>**<a href="**"></p> CURRENT', () => {
+    let html = '<p>**<a href="**"></p>';
+    let parser = new Parser(html);
+    let parsedHtml = parser.parse();
+
+    let htmlAtJSON = new AtJSON({
+      content: html,
+      contentType: 'text/html',
+      annotations: parsedHtml
+    });
+    console.log(htmlAtJSON);
+
+    let hir = new HIR(htmlAtJSON).toJSON();
+    expect(hir).toEqual({
+      type: 'root',
+      attributes: undefined,
+      children: [{
+        type: 'paragraph',
+        attributes: {},
+        children: [
+          '**',
+          { type: 'a', attributes: {}, children: [] }
+        ]
+      }]
+    });
+  });
 });
