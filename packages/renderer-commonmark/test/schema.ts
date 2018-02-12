70ad3b0ee3fb50d21d0df7f005a58cfa0987508d
Tim Evans <tim_evans@condenast.com>
2018-02-12 14:23:11 -0500

    add schema for commonmark renderer tests

---
+++ packages/renderer-commonmark/test/schema.ts
@@ -0,0 +1,44 @@
+export default {
+  'link': {
+    type: 'inline'
+  },
+  'paragraph': {
+    type: 'paragraph'
+  },
+  'heading': {
+    type: 'block'
+  },
+  'list-item': {
+    type: 'block'
+  },
+  'ordered-list': {
+    type: 'block'
+  },
+  'unordered-list': {
+    type: 'block'
+  },
+  'bold': {
+    type: 'inline'
+  },
+  'italic': {
+    type: 'inline'
+  },
+  'code-block': {
+    type: 'block'
+  },
+  'code': {
+    type: 'inline'
+  },
+  'blockquote': {
+    type: 'block'
+  },
+  'horizontal-rule': {
+    type: 'object'
+  },
+  'line-break': {
+    type: 'object'
+  },
+  'image': {
+    type: 'object'
+  }
+};
