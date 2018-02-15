c3b7d3172e5a115e4235a17230e264e7cb2e4851
Tim Evans <tim_evans@condenast.com>
2018-02-15 17:41:16 -0500

    don't compare the source parsing against the commonmark-spec; instead we're testing this in the renderer

---
+++ packages/source-commonmark/test/commonmark-test.ts
@@ -0,0 +1,42 @@
+import Document from '@atjson/document';
+import { HIR } from '@atjson/hir';
+import Renderer from '@atjson/renderer-hir';
+import CommonMarkSource, { schema } from '@atjson/source-commonmark';
+
+export default class PlainTextRenderer extends Renderer {
+  *root() {
+    let text: string[] = yield;
+    return text.join('');
+  }
+  *hardbreak() {
+    return '\n';
+  }
+  *paragraph() {
+    let text = yield;
+    return `${text.join('')}\n\n`;
+  }
+}
+
+function render(doc) {
+  let renderer = new PlainTextRenderer();
+  return renderer.render(doc);
+}
+
+describe('whitespace', () => {
+  test('&nbsp; is translated to a non-breaking space', () => {
+    let doc = new CommonMarkSource('&nbsp;');
+    expect(render(doc)).toBe('\u00A0\n\n');
+  });
+
+  test('  \\n is converted to a hardbreak', () => {
+    let doc = new CommonMarkSource('1  \n2');
+    expect(render(doc)).toBe('1\n2\n\n');
+  });
+});
+
+describe('code blocks', () => {
+  test('` `` ` is converted to an inline code block', () => {
+    let doc = new CommonMarkSource('` `` `');
+    expect(render(doc)).toBe(' `` \n\n');
+  });
+});
