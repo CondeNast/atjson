e1571ee03807fb7162389a0bd2b9eeb670731701
Blaine Cook <blaine_cook@condenast.com>
2018-03-07 17:37:57 +0000

    Add test of toCommonSchema() for GDocs Source

---
+++ packages/source-gdocs-paste/test/atjson-source-gdocs-translator-test.ts
@@ -0,0 +1,40 @@
+import GDocsSource from '@atjson/source-gdocs-paste';
+import * as fs from 'fs';
+import * as path from 'path';
+
+describe('@atjson/source-gdocs-paste', () => {
+  var atjson;
+
+  beforeAll(() => {
+    let fixturePath = path.join(__dirname, 'fixtures', 'complex.json');
+    let rawJSON = JSON.parse(fs.readFileSync(fixturePath));
+    let gdocs = new GDocsSource(rawJSON);
+    atjson = gdocs.toCommonSchema();
+  });
+
+  it('correctly converts -gdocs-ts_bd to bold', () => {
+    let bolds = atjson.annotations.filter(a => a.type === 'bold');
+    expect(bolds.length).toEqual(2);
+  });
+
+  it('correctly converts italic', () => {
+    let italics = atjson.annotations.filter(a => a.type === 'italic');
+    expect(italics.length).toEqual(2);
+  });
+
+  it('correctly converts headings', () => {
+    let headings = atjson.annotations.filter(a => a.type === 'heading');
+    expect(headings.length).toEqual(4);
+    expect(headings.map(h => h.attributes.level)).toEqual([1, 2, 100, 101]);
+  });
+
+  it('correctly converts lists', () => {
+    let lists = atjson.annotations.filter(a => a.type === 'list');
+    expect(lists.length).toEqual(1);
+  });
+
+  it('correctly converts list-items', () => {
+    let listItems = atjson.annotations.filter(a => a.type === 'list-item');
+    expect(listItems.length).toEqual(2);
+  });
+});
