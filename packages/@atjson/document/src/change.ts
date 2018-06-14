e71ad2ddeb30810b489d79048da80d0895e3108c
Tim Evans <tim_evans@condenast.com>
2018-06-14 16:09:55 -0400

    🚥  fix tests for insertText / deleteText

---
+++ packages/@atjson/document/src/change.ts
@@ -0,0 +1,31 @@
+export default abstract class Change {
+  abstract readonly type: string;
+}
+
+export enum AdjacentBoundaryBehaviour {
+  preserve,
+  default,
+  modify
+}
+
+export class Deletion extends Change {
+  readonly type = 'deletion';
+  constructor(
+    readonly start: number,
+    readonly end: number,
+    readonly behaviour: AdjacentBoundaryBehaviour = AdjacentBoundaryBehaviour.default
+  ) {
+    super();
+  }
+}
+
+export class Insertion extends Change {
+  readonly type = 'insertion';
+  constructor(
+    readonly start: number,
+    readonly text: string,
+    readonly behaviour: AdjacentBoundaryBehaviour = AdjacentBoundaryBehaviour.default
+  ) {
+    super();
+  }
+}
