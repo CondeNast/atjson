804be328549621887a110ba7b0d6df8277e7b5a5
Tim Evans <tim_evans@condenast.com>
2018-06-20 10:22:17 -0400

    😵 fix class name of object annotation

--- packages/@atjson/document/src/object-annotation.ts
+++ packages/@atjson/document/src/object-annotation.ts
@@ -1,6 +1,6 @@
 import Annotation from './annotation';
 
-export default abstract class BlockAnnotation extends Annotation {
+export default abstract class ObjectAnnotation extends Annotation {
   rank() {
     return 1000;
   }
