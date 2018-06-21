f34aefbde51b3b8ea5b7c8804d29aa9f25b75fa2
Blaine Cook <blaine_cook@condenast.com>
2018-06-22 00:07:24 +0100

    add toolbar that maps to selections

---
+++ packages/@atjson/editor/src/selection-toolbar.ts
@@ -0,0 +1,44 @@
+import WebComponent from './mixins/component';
+
+export default class SelectionToolbar extends WebComponent {
+  static template = '<button data-type="link">link</button><button data-type="bold"><b>b</b></button><button data-type="italic"><i>i</i></button><button data-type="strikethrough"><del>s</del></button>';
+
+  static style = `
+    :host {
+      border-radius: 4px;
+    }
+  `;
+
+  static events = {
+    'click': 'onClick'
+  }
+
+  onClick(evt) {
+    let target = null;
+    for (var i = 0; i < evt.path.length; i++) {
+      console.log('got target', target);
+      if (evt.path[i].nodeName === 'BUTTON') {
+        target = evt.path[i];
+        break;
+      }
+    }
+
+    let type = target.getAttribute('data-type');
+    console.log('got a click', evt, this.getAttribute('start'), this.getAttribute('end'), type);
+    let detail = {
+      type,
+      start: parseInt(this.getAttribute('start')),
+      end: parseInt(this.getAttribute('end'))
+    };
+
+    if (type === 'link') {
+      detail.attributes = { url: '' }
+    }
+
+    this.dispatchEvent(new CustomEvent('addAnnotation', { bubbles: true, detail }));
+  }
+}
+
+if (!window.customElements.get('selection-toolbar')) {
+  window.customElements.define('selection-toolbar', SelectionToolbar);
+}
