c7684e7646737fe22a13c219666f21c969da47ec
Blaine Cook <blaine_cook@condenast.com>
2018-06-27 16:10:15 -0400

    tweak logo

--- packages/@atjson/editor/src/editor-demo/logo.ts
+++ packages/@atjson/editor/src/editor-demo/logo.ts
@@ -5,11 +5,21 @@ export default class OffsetLogo extends WebComponent {
 
   static style = 'canvas { width: 300px; height: 150px; }';
 
-  static observedAttributes = ['offset'];
+  static observedAttributes = ['offset', 'continuous'];
 
-  render() {
-    let x = parseInt(this.getAttribute('offset'));
+  initChannel(color, initialRotation, initialOffset, rotationAngleMultiplier, magicNumber) {
+		let rotationAngle = (Math.abs(Math.cos(1 * Math.PI / 180.0)) + 1) * 2/3;
+    return {
+      color,
+      point: this.rotatePoint(initialRotation, { x: this.origin.x + initialOffset.x, y: this.origin.y + initialOffset.x }),
+      angle: rotationAngle * rotationAngleMultiplier,
+      dx: 0,
+      dtheta: 0,
+      magicNumber
+    }
+  }
 
+  initCanvas() {
     var canvas = this.shadowRoot.querySelector('canvas');
     canvas.width = 300;
     canvas.height = 150;
@@ -21,51 +31,98 @@ export default class OffsetLogo extends WebComponent {
 
     ctx.globalCompositeOperation = 'darken';
     let fontSize = canvas.width / 7 + 'px';
-    ctx.font = `${fontSize} Helvetica`;
+    ctx.font = `italic ${fontSize} Georgia`;
 
-		var origin = { x: 2, y: canvas.height / 4.0 };
+		this.origin = { x: 5, y: canvas.height / 4.0 };
+    this.ctx = ctx;
 
-		let rotatePoint = (angle, point) => {
-			var radians = angle * Math.PI / 180.0;
-			var cos = Math.cos(radians);
-			var sin = Math.sin(radians);
-			var dX = point.x - origin.x;
-			var dY = point.y - origin.y;
-			var x = cos * dX - sin * dY + origin.x;
-			var y = sin * dX + cos * dY + origin.y;
-			return { x, y };
-		}
+    this.channels = [
+      this.initChannel('cyan', 0, { x: -2, y: -2 }, 1, 5),
+      this.initChannel('magenta', 120, { x: -2, y: -2 }, -1, 95),
+      this.initChannel('yellow', 240, { x: -2, y: -2 }, 1, 50)
+    ];
+  }
 
-    if (!this.cyanPoint) {
-			this.cyanPoint = { x: 0, y: canvas.height / 4.0 - 1 };
-			this.magentaPoint = rotatePoint(120, { x: 0, y: canvas.height / 4.0 - 1 });
-			this.yellowPoint = rotatePoint(240, { x: 0, y: canvas.height / 4.0 - 1 });
+  rotatePoint(angle, point) {
+    var radians = angle * Math.PI / 180.0;
+    var cos = Math.cos(radians);
+    var sin = Math.sin(radians);
+    var dX = point.x - this.origin.x;
+    var dY = point.y - this.origin.y;
+    var x = cos * dX - sin * dY + this.origin.x;
+    var y = sin * dX + cos * dY + this.origin.y;
+    return { x, y };
+  }
+
+	drawOffset(point, color) {
+    this.ctx.fillStyle = color;
+    this.ctx.fillText('offset', point.x, point.y);
+  }
+
+  mutateChannel(channel) {
+    var thetaSign = 1;
+    if (Math.random() < 0.25) thetaSign = -1;
+
+    if (channel.dtheta < 1 && channel.dtheta > -1) {
+      channel.dtheta += thetaSign * Math.random() 
     }
-		var rotationAngle = 2 * (Math.abs(Math.cos(1 * Math.PI / 180.0)) + 1);
 
-		let drawOffset = (point, color) => {
-			var newPoint = rotatePoint(rotationAngle, point);
-			ctx.fillStyle = color;
-			ctx.fillText('Offset', newPoint.x, newPoint.y);
-			return newPoint;
-		}
+    if (channel.angle < 0.05 && channel.angle > 0 ) {
+      channel.angle = -0.05;
+    } else if (channel.angle < 0 && channel.angle > -0.05) {
+      channel.angle = 0.05;
+    }
 
-		let drawOffsets = (angle) => {
-			ctx.clearRect(0, 0, 1000, 300);
-			console.log(angle, this.cyanPoint);
-			this.cyanPoint = drawOffset(this.cyanPoint, 'cyan');
-			this.magentaPoint = drawOffset(this.magentaPoint, 'magenta');
-			this.yellowPoint = drawOffset(this.yellowPoint, 'yellow');
-		}
+    var newAngle = channel.dtheta * channel.angle;
+    if (newAngle < 5 && newAngle > -5) {
+      channel.angle = newAngle;
+    }
 
-    drawOffsets(x);
+    if (Math.random() < 0.5 && channel.dx < 2.5) {
+      channel.point.x += 0.5 / 3;
+      channel.dx += 0.5 / 3;
+    } else if (channel.dx > -2.5) {
+      channel.point.x -= 0.5 / 3;
+      channel.dx -= 0.5 / 3;
+    }
+
+    return channel;
+  }
+
+  drawChannel(channel) {
+		let rotationAngle = (Math.abs(Math.cos(1 * Math.PI / 180.0)) + 1) * 2/3;
+    channel.point = this.rotatePoint(rotationAngle, channel.point);
+    this.drawOffset(channel.point, channel.color);
+
+    if (Math.floor(Math.random()*100) !== channel.magicNumber) {
+      return channel;
+    } else {
+      return this.mutateChannel(channel);
+    }
+  }
+
+  render() {
+    this.ctx.clearRect(0, 0, 1000, 300);
+    this.channels.map(c => this.drawChannel(c));
   }
 
   attributeChangedCallback(attribute) {
+    if (!this.ctx) return;
+
     this.render();
+
+    if (this.hasAttribute('continuous') && !this.loop) {
+      let loop = () => {
+        this.render();
+        window.requestAnimationFrame(loop);
+      }
+      loop();
+      this.loop = true;
+    }
   }
 
   connectedCallback() {
+    this.initCanvas();
     this.render();
   }
 }
