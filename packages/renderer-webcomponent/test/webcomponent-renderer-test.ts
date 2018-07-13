e6e1ea6f3d0dbe515b0312f49c80791944153dfb
Blaine Cook <romeda@gmail.com>
2018-07-13 14:34:12 +0100

    Delete unused webcomponent renderer, pending proper extraction from editor.

--- packages/renderer-webcomponent/test/webcomponent-renderer-test.ts
+++
@@ -1,6 +0,0 @@
-import Document, { Annotation } from '@atjson/document';
-import WebComponentRenderer from '@atjson/renderer-webcomponent';
-
-describe('PlainTextRenderer', function () {
-  it('renders the atjson document');
-});
