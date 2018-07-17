451e5b9f0efa55cd761efe4b102eab74a9bce427
Blaine Cook <romeda@gmail.com>
2018-07-17 01:04:25 +0100

    tslint cleanups

--- packages/@atjson/editor/src/components/logo.ts
+++ packages/@atjson/editor/src/components/logo.ts
@@ -1,14 +1,18 @@
 import WebComponent from '../mixins/component';
 
 export default class OffsetLogo extends WebComponent {
+
   static template = '<canvas></canvas>';
-
   static style = 'canvas { width: 300px; height: 150px; }';
-
   static observedAttributes = ['offset', 'continuous'];
 
+  ctx: CanvasRenderingContext2D;
+  origin: { x: number, y: number };
+  loop?: boolean;
+  channels?: any[];
+
   initChannel(color, initialRotation, initialOffset, rotationAngleMultiplier, magicNumber) {
-		let rotationAngle = (Math.abs(Math.cos(1 * Math.PI / 180.0)) + 1) * 2/3;
+    let rotationAngle = (Math.abs(Math.cos(1 * Math.PI / 180.0)) + 1) * 2 / 3;
     return {
       color,
       point: this.rotatePoint(initialRotation, { x: this.origin.x + initialOffset.x, y: this.origin.y + initialOffset.x }),
@@ -16,24 +20,26 @@ export default class OffsetLogo extends WebComponent {
       dx: 0,
       dtheta: 0,
       magicNumber
-    }
+    };
   }
 
   initCanvas() {
-    var canvas = this.shadowRoot.querySelector('canvas');
+    let canvas = this.shadowRoot.querySelector('canvas');
+    if (canvas === null) return;
     canvas.width = 300;
     canvas.height = 150;
-    canvas.style.width = 150;
-    canvas.style.height = 75;
+    canvas.style.width = '150';
+    canvas.style.height = '75';
 
-    var ctx = canvas.getContext('2d');
+    let ctx = canvas.getContext('2d');
+    if (ctx === null) throw new Error('Unable to initialize Canvas');
     ctx.scale(2, 2);
 
     ctx.globalCompositeOperation = 'darken';
     let fontSize = canvas.width / 7 + 'px';
     ctx.font = `italic ${fontSize} Georgia`;
 
-		this.origin = { x: 5, y: canvas.height / 4.0 };
+    this.origin = { x: 5, y: canvas.height / 4.0 };
     this.ctx = ctx;
 
     this.channels = [
@@ -43,28 +49,28 @@ export default class OffsetLogo extends WebComponent {
     ];
   }
 
-  rotatePoint(angle, point) {
-    var radians = angle * Math.PI / 180.0;
-    var cos = Math.cos(radians);
-    var sin = Math.sin(radians);
-    var dX = point.x - this.origin.x;
-    var dY = point.y - this.origin.y;
-    var x = cos * dX - sin * dY + this.origin.x;
-    var y = sin * dX + cos * dY + this.origin.y;
+  rotatePoint(angle: number, point) {
+    let radians = angle * Math.PI / 180.0;
+    let cos = Math.cos(radians);
+    let sin = Math.sin(radians);
+    let dX = point.x - this.origin.x;
+    let dY = point.y - this.origin.y;
+    let x = cos * dX - sin * dY + this.origin.x;
+    let y = sin * dX + cos * dY + this.origin.y;
     return { x, y };
   }
 
-	drawOffset(point, color) {
+  drawOffset(point, color) {
     this.ctx.fillStyle = color;
     this.ctx.fillText('offset', point.x, point.y);
   }
 
   mutateChannel(channel) {
-    var thetaSign = 1;
+    let thetaSign = 1;
     if (Math.random() < 0.25) thetaSign = -1;
 
     if (channel.dtheta < 1 && channel.dtheta > -1) {
-      channel.dtheta += thetaSign * Math.random() 
+      channel.dtheta += thetaSign * Math.random();
     }
 
     if (channel.angle < 0.05 && channel.angle > 0 ) {
@@ -73,7 +79,7 @@ export default class OffsetLogo extends WebComponent {
       channel.angle = 0.05;
     }
 
-    var newAngle = channel.dtheta * channel.angle;
+    let newAngle = channel.dtheta * channel.angle;
     if (newAngle < 5 && newAngle > -5) {
       channel.angle = newAngle;
     }
@@ -89,12 +95,12 @@ export default class OffsetLogo extends WebComponent {
     return channel;
   }
 
-  drawChannel(channel) {
-    let rotationAngle = (Math.abs(Math.cos(1 * Math.PI / 180.0)) + 1) * 2/3;
+  drawChannel(channel: any) {
+    let rotationAngle = (Math.abs(Math.cos(1 * Math.PI / 180.0)) + 1) * 2 / 3;
     channel.point = this.rotatePoint(rotationAngle, channel.point);
     this.drawOffset(channel.point, channel.color);
 
-    if (Math.floor(Math.random()*100) !== channel.magicNumber) {
+    if (Math.floor(Math.random() * 100) !== channel.magicNumber) {
       return channel;
     } else {
       return this.mutateChannel(channel);
@@ -106,7 +112,7 @@ export default class OffsetLogo extends WebComponent {
     this.channels.map(c => this.drawChannel(c));
   }
 
-  attributeChangedCallback(attribute) {
+  attributeChangedCallback() {
     if (!this.ctx) return;
 
     this.render();
@@ -115,7 +121,7 @@ export default class OffsetLogo extends WebComponent {
       let loop = () => {
         this.render();
         window.requestAnimationFrame(loop);
-      }
+      };
       loop();
       this.loop = true;
     }
