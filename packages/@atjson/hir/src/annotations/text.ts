77350f0855a5a16d601bb42e71322aa7df3a188d
Tim Evans <tim_evans@condenast.com>
2018-07-17 14:08:48 -0400

    add annotations for root and text for the HIR

---
+++ packages/@atjson/hir/src/annotations/text.ts
@@ -0,0 +1,13 @@
+import { Annotation } from '@atjson/document';
+
+export default class TextAnnotation extends Annotation {
+  static vendorPrefix = 'atjson';
+  static type = 'text';
+  attributes: {
+    text: string;
+  };
+
+  get rank() {
+    return Infinity;
+  }
+}
