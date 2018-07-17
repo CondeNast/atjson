4ab563503fc19699bb42fbf812eedd18359757f0
Blaine Cook <romeda@gmail.com>
2018-07-17 01:04:25 +0100

    tslint cleanups

--- packages/@atjson/editor/src/editor-demo/index.ts
+++ packages/@atjson/editor/src/editor-demo/index.ts
@@ -1,10 +1,10 @@
 import Document from '@atjson/document';
-import Editor from '../index';
-import WebComponentRenderer from '../webcomponent-renderer';
 import CommonmarkRenderer from '@atjson/renderer-commonmark';
-import InspectorGadget from '../inspector-gadget';
 import OffsetLogo from '../components/logo';
+import Editor from '../index';
+import InspectorGadget from '../inspector-gadget';
 import events from '../mixins/events';
+import WebComponentRenderer from '../webcomponent-renderer';
 
 import EditableLink from '../components/editable-link';
 
@@ -22,41 +22,52 @@ if (!window.customElements.get('offset-logo')) {
 
 export default class EditorDemo extends events(HTMLElement) {
   static template = '<h1><offset-logo offset="0"></offset-logo></h1><text-editor></text-editor>' +
-                    '<h1>HTML Output</h1><div class="output"></div>' + 
+                    '<h1>HTML Output</h1><div class="output"></div>' +
                     '<h1>Commonmark Output</h1><div class="markdown"></div>' +
                     '<inspector-gadget></inspector-gadget>';
 
   static events = {
-    'change text-editor'(evt) {
+    'change text-editor'(evt: CustomEvent) {
       this.renderOutput(evt.detail.document);
       this.renderMarkdown(evt.detail.document);
     }
-  }
+  };
 
-  renderOutput(doc) {
+  renderOutput(doc: Document) {
     let outputElement = this.querySelector('.output');
     let rendered = new WebComponentRenderer(doc).render();
-    outputElement.innerHTML = rendered.innerHTML;
+    if (outputElement) {
+      outputElement.innerHTML = rendered.innerHTML;
+    }
   }
 
-  renderMarkdown(doc) {
+  renderMarkdown(doc: Document) {
     let outputElement = this.querySelector('.markdown');
     let rendered = new CommonmarkRenderer().render(doc);
-    outputElement.innerHTML = rendered;
+    if (outputElement) {
+      outputElement.innerHTML = rendered;
+    }
   }
 
   setDocument(doc: Document) {
     let editor = this.querySelector('text-editor');
 
+    if (editor === null) return;
+
     editor.setDocument(doc);
 
     let inspectorGadget = this.querySelector('inspector-gadget');
-    inspectorGadget.setDocument(doc);
-    inspectorGadget.setSelection(editor.getSelection());
 
-    doc.addEventListener('change', _ => {
-			let logo = this.querySelector('offset-logo');
-			logo.setAttribute('offset', doc.content.length)
+    if (inspectorGadget) {
+      inspectorGadget.setDocument(doc);
+      inspectorGadget.setSelection(editor.getSelection());
+    }
+
+    doc.addEventListener('change', () => {
+      let logo = this.querySelector('offset-logo');
+      if (logo) {
+         logo.setAttribute('offset', doc.content.length);
+      }
     });
   }
 
@@ -64,7 +75,10 @@ export default class EditorDemo extends events(HTMLElement) {
     this.innerHTML = this.constructor.template;
 
     let editor = this.querySelector('text-editor');
-    editor.addContentFeature(EditableLink);
+
+    if (editor) {
+      editor.addContentFeature(EditableLink);
+    }
 
     super.connectedCallback();
   }
