c07ae746aa4ac0e3bb57830430d2b96b41b23e10
Blaine Cook <blaine_cook@condenast.com>
2018-08-06 22:38:56 +0100

    giving up. switching to a local server. shrug.

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-#!/usr/bin/env node
+const appName = require('./package').name;
+const express = require('express');
+const app = express();
 
-const { execSync } = require('child_process');
+const basePath = '/packages/@atjson/offset-inspector/dist';
 
-const port = process.env.NODE_PORT || '8081';
+app.use(express.static(__dirname + basePath, {
+  index: false
+}));
 
-console.log('Listening on ', port);
+// /ping for a healthy app!
+app.get('/ping', (req, res) => res.send(200));
 
-process.chdir("./packages/@atjson/offset-inspector/");
-console.log('current directory ', process.cwd());
-execSync(`./node_modules/.bin/parcel serve --port ${port} public/index.html`);
+let index = require('fs').readFileSync(__dirname + basePath + '/index.html').toString();
+//let environment = require('./config/environment')(process.env['NODE_ENV'] || 'development');
+
+// Dynamic configuration is stored in a meta tag named "${package.name}/config"
+/*let metaTag = new RegExp(`name="${appName}/config/environment" content="(.*)"`);
+if (index.match(metaTag) == null) {
+  console.warn('');
+}
+
+index = index.replace(metaTag, function (match) {
+  return `name="${appName}/config/environment" content="${encodeURIComponent(JSON.stringify(environment))}"`;
+});
+*/
+
+// Return the index.html with appropriate configuration variables
+app.get('*', function (req, res) {
+  res.send(index);
+});
+
+if (process.env['NODE_PORT']) {
+  let port = process.env['NODE_PORT'];
+  app.listen(port, function () {
+    console.log(`${appName} listening on port ${port}`);
+  });
+}
+
+module.exports = app;
