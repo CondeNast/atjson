5c2718b762baddd05c843fa782271540b95f9ec7
Blaine Cook <blaine_cook@condenast.com>
2018-08-06 20:05:39 +0100

    debugging

--- index.js
+++ index.js
@@ -7,4 +7,5 @@ const port = process.env.NODE_PORT || '8081';
 console.log('Listening on ', port);
 
 process.chdir("./packages/@atjson/offset-inspector/");
+console.log('current directory ', process.cwd());
 execSync(`./node_modules/.bin/parcel serve --port ${port} public/index.html`);
