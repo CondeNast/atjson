dece44d5d88bb76369884f33259a14839759de9c
Blaine Cook <blaine_cook@condenast.com>
2018-08-31 22:16:02 +0200

    Move events to named functions

--- packages/@atjson/editor/src/index.ts
+++ packages/@atjson/editor/src/index.ts
@@ -5,113 +5,28 @@ import './selection-toolbar';
 import './text-input';
 import './text-selection';
 
-interface Range {
+export interface Range {
   start: number;
   end: number;
 }
 
 export default class OffsetEditor extends events(HTMLElement) {
 
-  static template = '<text-input><text-selection><selection-toolbar slot="toolbar"></selection-toolbar><div class="editor" style="white-space: pre-wrap; padding: 1em; border: 1px solid black; border-radius: 5px; outline: none; font-size: 1.5em;" contenteditable></div></text-selection></text-input>';
+  static template = '<text-input>' +
+                      '<text-selection>' +
+                        '<selection-toolbar slot="toolbar"></selection-toolbar>' +
+                        '<div class="editor" style="white-space: pre-wrap" contenteditable></div>' +
+                      '</text-selection>' +
+                    '</text-input>';
 
   static events = {
-    'change text-selection'(evt: CustomEvent) {
-      this.selection = evt.detail;
-      let toolbar = this.querySelector('selection-toolbar');
-
-      let selectedAnnotations = this.document.annotations.filter((a: Annotation) => {
-        return (a.start <= evt.detail.start && a.end >= evt.detail.start) ||
-               (a.start <= evt.detail.end && a.end >= evt.detail.end);
-      });
-
-      let selectionChangeEvent = new CustomEvent('selectionchange', {
-        detail: Object.assign({
-          selectedAnnotations,
-          document: this.document
-        }, evt.detail),
-        bubbles: false
-      });
-      toolbar.dispatchEvent(selectionChangeEvent);
-    },
-
-    'insertText text-input'(evt: CustomEvent) {
-      this.document.insertText(evt.detail.position, evt.detail.text);
-      this.selection.start += evt.detail.text.length;
-      this.selection.end += evt.detail.text.length;
-    },
-
-    'deleteText text-input'(evt: CustomEvent) {
-      let deletion = evt.detail;
-      this.document.deleteText(deletion);
-      // FIXME the selection should just be an annotation that we transform. We shouldn't handle logic here.
-      if (this.selection.start < deletion.start) {
-        // do nothing.
-      } else if (this.selection.start < deletion.end) {
-        this.selection.start = this.selection.end = deletion.start;
-      } else {
-        let l = deletion.end - deletion.start;
-        this.selection.start -= l;
-        this.selection.end -= l;
-      }
-    },
-
-    'replaceText text-input'(evt: CustomEvent) {
-      let replacement = evt.detail;
-
-      this.document.deleteText(replacement);
-      this.document.insertText(replacement.start, replacement.text);
-      this.selection.start = replacement.start + replacement.text.length;
-    },
-
-    'addAnnotation'(evt: CustomEvent) {
-      if (evt.detail.type === 'bold' || evt.detail.type === 'italic') {
-
-        const contained = (a: Annotation, b: Annotation) => a.start >= b.start && a.end <= b.end;
-        const offset = (a: Annotation, b: Annotation) => a.start <= b.end && a.end >= b.start;
-        let overlapping = this.document.annotations.filter((a: Annotation) => a.type === evt.detail.type)
-                                                   .filter((a: Annotation) => contained(a, evt.detail) || contained(evt.detail, a) || offset(a, evt.detail) || offset(evt.detail, a));
-
-        let min = overlapping.reduce((a: number, b: Annotation) => Math.min(a, b.start), this.document.content.length);
-        let max = overlapping.reduce((a: number, b: Annotation) => Math.max(a, b.end), 0);
-
-        if (overlapping.length === 0) {
-          this.document.addAnnotations(evt.detail);
-
-        } else if (min <= evt.detail.start && evt.detail.end <= max && overlapping.length === 1) {
-          // invert the state.
-          let prev = overlapping[0];
-          let newFirst = Object.assign({}, prev, evt.detail, { start: prev.start, end: evt.detail.start });
-          let newLast = Object.assign({}, prev, evt.detail, { start: evt.detail.end, end: prev.end });
-          if (min !== evt.detail.start) this.document.addAnnotations(newFirst);
-          if (max !== evt.detail.end) this.document.addAnnotations(newLast);
-
-        } else {
-          this.document.addAnnotations(Object.assign({}, overlapping[0], evt.detail, { start: Math.min(min, evt.detail.start), end: Math.max(max, evt.detail.end) }));
-        }
-
-        overlapping.forEach((o: Annotation) => this.document.removeAnnotation(o));
-
-      } else {
-        this.document.addAnnotations(evt.detail);
-      }
-    },
-
-    'deleteAnnotation'(evt: CustomEvent) {
-      let annotation = this.document.annotations.find((a: Annotation) => a.id === evt.detail.annotationId);
-      this.document.removeAnnotation(annotation);
-    },
-
-    'attributechange'(evt: CustomEvent) {
-      if (evt.detail.annotationId) {
-        let annotation = this.document.annotations.find((a: Annotation) => a.id === evt.detail.annotationId);
-        this.document.replaceAnnotation(annotation, Object.assign(annotation, {attributes: evt.detail.attributes}));
-      } else if (evt.target !== null) {
-        let annotationId = evt.target.getAttribute('data-annotation-id');
-        let annotation = this.document.annotations.find((a: Annotation) => a.id.toString(10) === annotationId);
-        this.document.replaceAnnotation(annotation, Object.assign(annotation, evt.detail));
-      }
-    }
-
+    'change text-selection': 'handleTextSelectionChange',
+    'insertText text-input': 'handleTextInsert',
+    'deleteText text-input': 'handleTextDelete',
+    'replaceText text-input': 'handleTextReplace',
+    'addAnnotation': 'handleAnnotationAdd',
+    'deleteAnnotation': 'handleAnnotationDelete',
+    'attributechange': 'handleAttributeChange'
   };
 
   selection: Range | undefined;
@@ -169,8 +84,9 @@ export default class OffsetEditor extends events(HTMLElement) {
   }
 
   addContentFeature(component: any) {
-    if (component.selectionButton) {
-      this.querySelector('selection-toolbar').shadowRoot.appendChild(component.selectionButton);
+    const selectionToolbar = this.querySelector('selection-toolbar');
+    if (component.selectionButton && selectionToolbar instanceof Element) {
+      selectionToolbar.shadowRoot.appendChild(component.selectionButton);
     }
 
     if (component.annotationName) {
@@ -186,6 +102,103 @@ export default class OffsetEditor extends events(HTMLElement) {
     this.innerHTML = this.constructor.template;
     super.connectedCallback();
     this.scheduleRender();
+  }
+
+  handleTextSelectionChange(evt: CustomEvent) {
+    this.selection = evt.detail;
+    let toolbar = this.querySelector('selection-toolbar');
+
+    let selectedAnnotations = this.document.annotations.filter((a: Annotation) => {
+      return (a.start <= evt.detail.start && a.end >= evt.detail.start) ||
+              (a.start <= evt.detail.end && a.end >= evt.detail.end);
+    });
+
+    let selectionChangeEvent = new CustomEvent('selectionchange', {
+      detail: Object.assign({
+        selectedAnnotations,
+        document: this.document
+      }, evt.detail),
+      bubbles: false
+    });
+    toolbar.dispatchEvent(selectionChangeEvent);
+  }
+
+  handleTextInsert(evt: CustomEvent) {
+    this.document.insertText(evt.detail.position, evt.detail.text);
+    this.selection.start += evt.detail.text.length;
+    this.selection.end += evt.detail.text.length;
+  }
+
+  handleTextDelete(evt: CustomEvent) {
+    let deletion = evt.detail;
+    this.document.deleteText(deletion);
+    // FIXME the selection should just be an annotation that we transform. We shouldn't handle logic here.
+    if (this.selection.start < deletion.start) {
+      // do nothing.
+    } else if (this.selection.start < deletion.end) {
+      this.selection.start = this.selection.end = deletion.start;
+    } else {
+      let l = deletion.end - deletion.start;
+      this.selection.start -= l;
+      this.selection.end -= l;
+    }
+  }
+
+  handleTextReplace(evt: CustomEvent) {
+    let replacement = evt.detail;
+
+    this.document.deleteText(replacement);
+    this.document.insertText(replacement.start, replacement.text);
+    this.selection.start = replacement.start + replacement.text.length;
+  }
+
+  handleAnnotationAdd(evt: CustomEvent) {
+    if (evt.detail.type === 'bold' || evt.detail.type === 'italic') {
+
+      const contained = (a: Annotation, b: Annotation) => a.start >= b.start && a.end <= b.end;
+      const offset = (a: Annotation, b: Annotation) => a.start <= b.end && a.end >= b.start;
+      let overlapping = this.document.annotations.filter((a: Annotation) => a.type === evt.detail.type)
+                                                  .filter((a: Annotation) => contained(a, evt.detail) || contained(evt.detail, a) || offset(a, evt.detail) || offset(evt.detail, a));
+
+      let min = overlapping.reduce((a: number, b: Annotation) => Math.min(a, b.start), this.document.content.length);
+      let max = overlapping.reduce((a: number, b: Annotation) => Math.max(a, b.end), 0);
+
+      if (overlapping.length === 0) {
+        this.document.addAnnotations(evt.detail);
+
+      } else if (min <= evt.detail.start && evt.detail.end <= max && overlapping.length === 1) {
+        // invert the state.
+        let prev = overlapping[0];
+        let newFirst = Object.assign({}, prev, evt.detail, { start: prev.start, end: evt.detail.start });
+        let newLast = Object.assign({}, prev, evt.detail, { start: evt.detail.end, end: prev.end });
+        if (min !== evt.detail.start) this.document.addAnnotations(newFirst);
+        if (max !== evt.detail.end) this.document.addAnnotations(newLast);
+
+      } else {
+        this.document.addAnnotations(Object.assign({}, overlapping[0], evt.detail, { start: Math.min(min, evt.detail.start), end: Math.max(max, evt.detail.end) }));
+      }
+
+      overlapping.forEach((o: Annotation) => this.document.removeAnnotation(o));
+
+    } else {
+      this.document.addAnnotations(evt.detail);
+    }
+  }
+
+  handleAnnotationDelete(evt: CustomEvent) {
+    let annotation = this.document.annotations.find((a: Annotation) => a.id === evt.detail.annotationId);
+    this.document.removeAnnotation(annotation);
+  }
+
+  handleAttributeChange(evt: CustomEvent) {
+    if (evt.detail.annotationId) {
+      let annotation = this.document.annotations.find((a: Annotation) => a.id === evt.detail.annotationId);
+      this.document.replaceAnnotation(annotation, Object.assign(annotation, {attributes: evt.detail.attributes}));
+    } else if (evt.target !== null) {
+      let annotationId = evt.target.getAttribute('data-annotation-id');
+      let annotation = this.document.annotations.find((a: Annotation) => a.id.toString() === annotationId);
+      this.document.replaceAnnotation(annotation, Object.assign(annotation, evt.detail));
+    }
   }
 }
 
