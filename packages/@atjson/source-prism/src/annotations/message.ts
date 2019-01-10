fa94c1c7c24cb7bf2777193b9d986b770a0b4893
Tim Evans <tim_evans@condenast.com>
2019-01-10 11:10:06 -0500

    fix parse issues with prism source and import annotations directly from the html source

--- packages/@atjson/source-prism/src/annotations/message.ts
+++ packages/@atjson/source-prism/src/annotations/message.ts
@@ -1,6 +1,6 @@
 import { BlockAnnotation } from '@atjson/document';
 
-export default class Media extends BlockAnnotation {
+export default class Message extends BlockAnnotation {
   static vendorPrefix = 'pam';
-  static type = 'media';
+  static type = 'message';
 }
