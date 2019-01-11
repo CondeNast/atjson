f3478bc8239bd295efd2dbd6b02e863d241999bc
Tim Evans <tim.c.evans@me.com>
2019-01-11 18:30:15 -0500

    update tester app for url source

--- packages/@atjson/source-url/public/app.js
+++ packages/@atjson/source-url/public/app.js
@@ -1,9 +1,13 @@
 import OffsetSource from '@atjson/offset-annotations';
 import URLSource from '../src';
 
-document.addEventListener('paste', (evt: ClipboardEvent) => {
+document.addEventListener('paste', (evt) => {
   let pasteBuffer = evt.clipboardData.getData('text/plain');
   if (pasteBuffer !== '') {
     document.body.innerText = JSON.stringify(URLSource.fromRaw(pasteBuffer).convertTo(OffsetSource).toJSON(), null, 2);
   }
+});
+
+document.querySelector('button').addEventListener('click', () => {
+  document.body.innerText = JSON.stringify(URLSource.fromRaw('https://twitter.com/jennschiffer/status/708888255828250625').convertTo(OffsetSource).toJSON(), null, 2);
 });
