8e2432fe2a1cd697d26c7834288dcb2d756445c9
Tim Evans <tim.c.evans@me.com>
2019-01-11 18:30:05 -0500

    update tester app for google docs paste

---
+++ packages/@atjson/source-gdocs-paste/public/app.js
@@ -0,0 +1,18 @@
+import GoogleDocsPasteSource from '../src';
+import * as fs from 'fs';
+
+const example = fs.readFileSync(__dirname + '/invisible-cities.json');
+
+document.addEventListener('paste', (evt) => {
+  let gdocsPaste = evt.clipboardData.getData('application/x-vnd.google-docs-document-slice-clip+wrapped');
+  if (gdocsPaste !== '') {
+    let data = JSON.parse(JSON.parse(gdocsPaste).data);
+    console.log(gdocsPaste);
+    document.body.innerText = JSON.stringify(GoogleDocsPasteSource.fromRaw(data).toJSON(), null, 2);
+  }
+});
+
+document.querySelector('button').addEventListener('click', () => {
+  let data = JSON.parse(JSON.parse(example.toString()).data);
+  document.body.innerText = JSON.stringify(GoogleDocsPasteSource.fromRaw(data).toJSON(), null, 2);
+});
