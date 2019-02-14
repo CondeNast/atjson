f6284dff4538475ea9a1c58f6b740e4221d00fec
Bach Bui <bach_bui@condenast.com>
2019-02-13 21:17:10 -0500

    Fix imports

--- packages/@atjson/offset-annotations/src/annotations/caption-source.ts
+++ packages/@atjson/offset-annotations/src/annotations/caption-source.ts
@@ -1,17 +1,15 @@
 import Document from '@atjson/document';
-import {
-  Bold,
-  Italic,
-  LineBreak,
-  Link,
-  List,
-  ListItem,
-  Paragraph,
-  Strikethrough,
-  Subscript,
-  Superscript,
-  Underline
-} from './index';
+import Bold from './bold';
+import Italic from './italic';
+import LineBreak from './line-break';
+import Link from './link';
+import List from './list';
+import ListItem from './list-item';
+import Paragraph from './paragraph';
+import Strikethrough from './strikethrough';
+import Subscript from './subscript';
+import Superscript from './superscript';
+import Underline from './underline';
 
 export default class CaptionSource extends Document {
   static schema = [
