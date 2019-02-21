37e119d2c542fc320c586069d51844fc7fa2ad30
Bach Bui <bachjbui@gmail.com>
2019-02-20 19:22:27 -0500

    🐝 Preserve space before sentence-terminating number (#114)
    * 🐝 Preserve leading spaces when escaping OL md text
    
    When escaping text that looks like a markdown ordered list, we want to
    preserve leading whitespace. This fixes a bug where a sentence ending
    with an italic and then a number would have the space between them
    removed, as the end of the sentence is rendered as text but looks like
    ordered list markdown:
    `Ending with *italic* 1. ` -> `Ending with *italic*1\.`
    We still have to escape the terminal period, but now it will render as
    `Ending with *italic* 1. ` -> `Ending with *italic* 1\.` with the space
    preserved.

--- packages/@atjson/renderer-commonmark/test/commonmark-test.ts
+++ packages/@atjson/renderer-commonmark/test/commonmark-test.ts
@@ -96,6 +96,19 @@ After all the lists
 `);
   });
 
+  test('preserve space between sentence-terminating italic + number.', () => {
+    let document = new OffsetSource({
+      content: 'Sentence ending in *italic* 1. New sentence',
+      annotations: [
+        { type: '-offset-italic', start: 19, end: 27, attributes: {} },
+        { type: '-atjson-parse-token', start: 19, end: 20, attributes: {} },
+        { type: '-atjson-parse-token', start: 26, end: 27, attributes: {} }
+      ]
+    });
+
+    expect(CommonmarkRenderer.render(document)).toBe('Sentence ending in *italic* 1\\. New sentence');
+  });
+
   test('links', () => {
     let document = new OffsetSource({
       content: 'I have a link',
