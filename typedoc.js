6b3d358711a16115778fd91933d09b87653eb3e0
Tim Evans <tim.c.evans@me.com>
2019-03-05 21:50:48 -0500

    update typedoc configuration so documentation builds properly

--- typedoc.js
+++ typedoc.js
@@ -1,11 +1,9 @@
 module.exports = {
   "src": [
     "./packages/@atjson/document/src",
-    "./packages/@atjson/editor/src",
     "./packages/@atjson/hir/src",
-    "./packages/@atjson/offset-annotation/src",
+    "./packages/@atjson/offset-annotations/src",
     "./packages/@atjson/offset-core-components/src",
-    "./packages/@atjson/offset-inspector/src",
     "./packages/@atjson/renderer-commonmark/src",
     "./packages/@atjson/renderer-graphviz/src",
     "./packages/@atjson/renderer-hir/src",
@@ -14,7 +12,10 @@ module.exports = {
     "./packages/@atjson/renderer-webcomponent/src",
     "./packages/@atjson/source-commonmark/src",
     "./packages/@atjson/source-gdocs-paste/src",
-    "./packages/@atjson/source-html/src"
+    "./packages/@atjson/source-html/src",
+    "./packages/@atjson/source-prism/src",
+    "./packages/@atjson/source-mobiledoc/src",
+    "./packages/@atjson/source-url/src"
   ],
   "out": "docs",
   "mode": "modules",
