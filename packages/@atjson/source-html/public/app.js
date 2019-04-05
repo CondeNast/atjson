178d1482192e20163ba4ab7e3d96fc4968c5fc94
Tim Evans <tim-evans@users.noreply.github.com>
2019-04-05 16:34:40 -0300

    ✨ Add public app to source-html (#123)

---
+++ packages/@atjson/source-html/public/app.js
@@ -0,0 +1,22 @@
+import OffsetSource from '@atjson/offset-annotations';
+import CommonmarkRenderer from '@atjson/renderer-commonmark';
+import HTMLSource from '../src';
+import * as fs from 'fs';
+
+const example = fs.readFileSync(__dirname + '/invisible-cities.html');
+
+document.addEventListener('paste', (evt) => {
+  let html = evt.clipboardData.getData('application/html') ||
+             evt.clipboardData.getData('text/plain');
+  if (html !== '') {
+    let htmlDoc = HTMLSource.fromRaw(html);
+    let commonmarkText = CommonmarkRenderer.render(htmlDoc.convertTo(OffsetSource));
+    document.body.innerText = commonmarkText + '\n\n-----------------\n\n' + JSON.stringify(htmlDoc.toJSON(), null, 2);
+  }
+});
+
+document.querySelector('button').addEventListener('click', () => {
+  let htmlDoc = HTMLSource.fromRaw(example.toString());
+  let commonmarkText = CommonmarkRenderer.render(htmlDoc.convertTo(OffsetSource));
+  document.body.innerText = commonmarkText + '\n\n-----------------\n\n' + JSON.stringify(htmlDoc.toJSON(), null, 2);
+});
