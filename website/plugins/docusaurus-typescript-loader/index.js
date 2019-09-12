f561ab06f2fe96dc807575ca40d9b11e5859592f
Tim Evans <tim.c.evans@me.com>
2019-09-12 11:26:13 -0400

    🐞 fix TypeScript loading in the website and use relative packages so we don't need to update them constantly

--- website/plugins/docusaurus-typescript-loader/index.js
+++ website/plugins/docusaurus-typescript-loader/index.js
@@ -1,7 +1,15 @@
 const path = require("path");
+const readFileSync = require("fs").readFileSync;
 
 module.exports = function(context) {
   const contentPath = path.join(path.resolve(context.siteDir), "src");
+  const package = JSON.parse(
+    readFileSync(path.join(path.resolve(context.siteDir), "package.json"))
+  );
+
+  const browserslist = package.browserslist.production
+    ? package.browserslist.production
+    : package.browserslist;
 
   return {
     name: "docusaurus-typescript-loader",
@@ -10,24 +18,49 @@ module.exports = function(context) {
       return [contentPath];
     },
 
-    configureWebpack() {
+    configureWebpack(_config, isServer, { getBabelLoader, getCacheLoader }) {
       return {
         module: {
           rules: [
             {
-              test: /(\.tsx?)$/,
-              include: [contentPath],
-              exclude: /node_modules/,
+              test: /\.jsx?$/,
               use: [
-                {
-                  loader: "ts-loader",
-                  options: {
-                    configFile: path.join(
-                      path.resolve(context.siteDir),
-                      "tsconfig.json"
-                    )
-                  }
-                }
+                getCacheLoader(isServer),
+                getBabelLoader(isServer, {
+                  presets: [
+                    [
+                      "@babel/env",
+                      {
+                        targets: browserslist
+                      }
+                    ],
+                    "@babel/react"
+                  ]
+                })
+              ]
+            },
+            {
+              test: /\.tsx?$/,
+              exclude: /node_modules/,
+              include: [contentPath],
+              use: [
+                getCacheLoader(isServer),
+                getBabelLoader(isServer, {
+                  presets: [
+                    [
+                      "@babel/preset-typescript",
+                      {
+                        isTSX: true,
+                        allExtensions: true
+                      }
+                    ],
+                    "@babel/preset-react"
+                  ],
+                  plugins: [
+                    "@babel/plugin-proposal-class-properties",
+                    "@babel/plugin-proposal-object-rest-spread"
+                  ]
+                })
               ]
             }
           ]
