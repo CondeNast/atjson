d1cbfe892faaa045952446dba0286654d7356261
Colin-Alexa Robinson <colin_robinson@condenast.com>
2019-12-13 16:07:44 -0500

    a different approach to the url converter

--- packages/@atjson/source-url/src/converter.ts
+++ packages/@atjson/source-url/src/converter.ts
@@ -1,23 +1,30 @@
 import OffsetSource, { SocialURLs } from "@atjson/offset-annotations";
+import { URLAnnotation } from "./annotations";
 import URLSource from "./source";
 
-URLSource.defineConverterTo(OffsetSource, function urlToOffset(doc) {
-  for (let annotation of doc.where({ type: "-whatwg-url" }).annotations) {
-    let { url, AnnotationClass } = SocialURLs.identify(annotation.attributes);
-    if (url && AnnotationClass) {
-      doc.replaceAnnotation(
-        annotation,
-        new AnnotationClass({
-          id: annotation.id,
-          start: annotation.start,
-          end: annotation.end,
-          attributes: {
-            url
-          }
-        })
-      );
-    }
-  }
+function isURL(annotation: URLAnnotation): annotation is URLAnnotation {
+  return annotation.type === "url";
+}
+
+URLSource.defineConverterTo(OffsetSource, doc => {
+  doc
+    .where(isURL)
+    .update(function identifyAndReplaceURL(annotation: URLAnnotation) {
+      let { url, AnnotationClass } = SocialURLs.identify(annotation.attributes);
+      if (url && AnnotationClass) {
+        doc.replaceAnnotation(
+          annotation,
+          new AnnotationClass({
+            id: annotation.id,
+            start: annotation.start,
+            end: annotation.end,
+            attributes: {
+              url
+            }
+          })
+        );
+      }
+    });
 
   return new OffsetSource(doc.toJSON());
 });
