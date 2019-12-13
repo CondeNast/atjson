24526e9effbc23172b539d598ec2ef17d4ac9701
Colin-Alexa Robinson <colin_robinson@condenast.com>
2019-12-12 19:21:30 -0500

    source-url

--- packages/@atjson/source-url/src/converter.ts
+++ packages/@atjson/source-url/src/converter.ts
@@ -1,26 +1,23 @@
 import OffsetSource, { SocialURLs } from "@atjson/offset-annotations";
-import { URLAnnotation } from "./annotations";
 import URLSource from "./source";
 
-URLSource.defineConverterTo(OffsetSource, doc => {
-  doc
-    .where((annotation: URLAnnotation) => annotation.type === "url")
-    .update((annotation: URLAnnotation) => {
-      let { url, AnnotationClass } = SocialURLs.identify(annotation.attributes);
-      if (url && AnnotationClass) {
-        doc.replaceAnnotation(
-          annotation,
-          new AnnotationClass({
-            id: annotation.id,
-            start: annotation.start,
-            end: annotation.end,
-            attributes: {
-              url
-            }
-          })
-        );
-      }
-    });
+URLSource.defineConverterTo(OffsetSource, function urlToOffset(doc) {
+  for (let annotation of doc.where({ type: "-whatwg-url" }).annotations) {
+    let { url, AnnotationClass } = SocialURLs.identify(annotation.attributes);
+    if (url && AnnotationClass) {
+      doc.replaceAnnotation(
+        annotation,
+        new AnnotationClass({
+          id: annotation.id,
+          start: annotation.start,
+          end: annotation.end,
+          attributes: {
+            url
+          }
+        })
+      );
+    }
+  }
 
   return new OffsetSource(doc.toJSON());
 });
