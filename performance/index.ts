33bec50f99c56403daeb4570a19c0f099257c54a
Bach Bui <bach_bui@condenast.com>
2019-12-18 20:39:45 -0500

    Declare perf test suites separately

--- performance/index.ts
+++ performance/index.ts
@@ -1,72 +1,63 @@
 /* eslint-env node */
 import * as spec from "commonmark-spec";
-import { profile } from "./src";
+import { profile, suite } from "./src";
+import { md } from "./fixtures";
 import CommonMarkSource from "@atjson/source-commonmark";
 import CommonMarkRenderer from "@atjson/renderer-commonmark";
 import OffsetSource from "@atjson/offset-annotations";
-import { readdirSync, readFileSync } from "fs";
 import * as minimist from "minimist";
-import { join } from "path";
+
+const TestSuites = [
+  suite({
+    name: "commonmark-spec",
+    cases: spec.tests,
+    runner: test => {
+      CommonMarkRenderer.render(
+        CommonMarkSource.fromRaw(test.markdown).convertTo(OffsetSource)
+      );
+    }
+  }),
+  suite({
+    name: "commonmark-spec equality",
+    cases: spec.tests,
+    runner: test => {
+      let doc = CommonMarkSource.fromRaw(test.markdown);
+      let md = CommonMarkRenderer.render(doc.convertTo(OffsetSource));
+      doc.equals(CommonMarkSource.fromRaw(md));
+    }
+  }),
+  suite({
+    name: "degenerate-markdown",
+    cases: md,
+    runner: markdown => {
+      CommonMarkRenderer.render(
+        CommonMarkSource.fromRaw(markdown).convertTo(OffsetSource)
+      );
+    }
+  }),
+  suite({
+    name: "degenerate-markdown equality",
+    cases: md,
+    runner: markdown => {
+      let doc = CommonMarkSource.fromRaw(markdown);
+      let md = CommonMarkRenderer.render(doc.convertTo(OffsetSource));
+      doc.equals(CommonMarkSource.fromRaw(md));
+    }
+  })
+];
 
 async function run() {
   const args = minimist(process.argv.slice(2), {
     string: ["baseline", "times"]
   });
-  const times = Number(args.times) || 10;
-  const baseline =
-    args.baseline || args.baseline === "" ? "baseline" : "current";
+  const options = {
+    runs: Number(args.times) || 10,
+    baseline: args.baseline || args.baseline === "" ? "baseline" : "current"
+  };
 
-  await profile(
-    "commonmark-spec",
-    baseline,
-    test => {
-      CommonMarkRenderer.render(
-        CommonMarkSource.fromRaw(test.markdown).convertTo(OffsetSource)
-      );
-    },
-    spec.tests,
-    times
-  );
-
-  await profile(
-    "commonmark-spec equality",
-    baseline,
-    test => {
-      let doc = CommonMarkSource.fromRaw(test.markdown);
-      let md = CommonMarkRenderer.render(doc.convertTo(OffsetSource));
-      doc.equals(CommonMarkSource.fromRaw(md));
-    },
-    spec.tests,
-    times
-  );
-
-  await profile(
-    "degenerate-markdown",
-    baseline,
-    markdown => {
-      CommonMarkRenderer.render(
-        CommonMarkSource.fromRaw(markdown).convertTo(OffsetSource)
-      );
-    },
-    readdirSync(join(__dirname, "fixtures")).map(filename =>
-      readFileSync(join(__dirname, "fixtures", filename)).toString()
-    ),
-    times
-  );
-
-  await profile(
-    "degenerate-markdown equality",
-    baseline,
-    markdown => {
-      let doc = CommonMarkSource.fromRaw(markdown);
-      let md = CommonMarkRenderer.render(doc.convertTo(OffsetSource));
-      doc.equals(CommonMarkSource.fromRaw(md));
-    },
-    readdirSync(join(__dirname, "fixtures")).map(filename =>
-      readFileSync(join(__dirname, "fixtures", filename)).toString()
-    ),
-    times
-  );
+  for (let suite of TestSuites) {
+    await profile<any>(suite, options);
+  }
 }
 
 run().then(
