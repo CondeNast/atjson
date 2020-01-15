98a4d3c7a0e10d159f394d0bc5ac37313dd4c7be
Bach Bui <bach_bui@condenast.com>
2020-01-15 18:40:06 -0500

    🚀 Use a more performant uuid library

--- __mocks__/uuid.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  v4() {
-    return 'Any<id>';
-  }
-};
--- packages/@atjson/document/src/annotation.ts
+++ packages/@atjson/document/src/annotation.ts
@@ -1,4 +1,4 @@
-import { v4 as uuid } from "uuid";
+import * as uuid from "uuid-random";
 import { clone, toJSON, unprefix } from "./attributes";
 import Change, {
   AdjacentBoundaryBehaviour,
