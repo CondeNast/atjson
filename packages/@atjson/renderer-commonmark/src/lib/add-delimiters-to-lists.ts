145a58bc7ea6b14e532568508f22ea1c127f631d
Tim Evans <tim.c.evans@me.com>
2020-02-07 09:52:25 -0500

    fix: fix indentations for sublists

--- packages/@atjson/renderer-commonmark/src/lib/add-delimiters-to-lists.ts
+++ packages/@atjson/renderer-commonmark/src/lib/add-delimiters-to-lists.ts
@@ -5,87 +5,99 @@ type TokenStream = Array<T.Token | string>;
 
 export function addDelimitersToLists(stream: TokenStream) {
   let result: TokenStream = [];
-  let delimiters: {
+  let markers: {
     delimiter: "." | ")" | "*" | "-";
     value?: number;
+    indent: number;
   }[] = [];
-  let lastDelimiter:
+  let lastMarker:
     | {
         delimiter: "." | ")" | "*" | "-";
         value?: number;
+        indent: number;
       }
     | undefined = undefined;
 
   for (let i = 0, len = stream.length; i < len; i++) {
     let current = stream[i];
     let previous = stream[i - 1];
+    let marker = markers[markers.length - 1];
 
     // Disambiguate between adjacent lists by rotating through
     // delimiters that are available as per the commonmark spec
     if (is(current, T.NumberedListStart)) {
-      if (is(previous, T.NumberedListEnd) && lastDelimiter) {
-        if (lastDelimiter.delimiter === ".") {
-          delimiters.unshift({
-            value: current.startsAt,
-            delimiter: ")"
-          });
-        } else {
-          delimiters.unshift({
-            value: current.startsAt,
-            delimiter: "."
-          });
-        }
-      } else {
-        delimiters.unshift({
+      let indent = marker?.indent || 0;
+
+      // We alternate between markers that start with
+      // . and ) (cf. 1. and 1)) for numbered lists
+      if (is(previous, T.NumberedListEnd) && lastMarker?.delimiter === ".") {
+        markers.push({
           value: current.startsAt,
-          delimiter: "."
+          delimiter: ")",
+          indent
+        });
+      } else {
+        markers.push({
+          value: current.startsAt,
+          delimiter: ".",
+          indent
         });
       }
     } else if (is(current, T.NumberedListEnd)) {
-      lastDelimiter = delimiters.shift();
+      lastMarker = markers.pop();
     }
 
     if (is(current, T.BulletedListStart)) {
-      if (is(previous, T.BulletedListEnd) && lastDelimiter) {
-        if (lastDelimiter.delimiter === "-") {
-          delimiters.unshift({
-            delimiter: "*"
-          });
-        } else {
-          delimiters.unshift({
-            delimiter: "-"
-          });
-        }
+      let indent = marker?.indent || 0;
+      // We alternate between markers that start with
+      // * and - for bulleted lists
+      if (is(previous, T.BulletedListEnd) && lastMarker?.delimiter === "-") {
+        markers.push({
+          delimiter: "*",
+          indent
+        });
       } else {
-        delimiters.unshift({
-          delimiter: "-"
+        markers.push({
+          delimiter: "-",
+          indent
         });
       }
     } else if (is(current, T.BulletedListEnd)) {
-      lastDelimiter = delimiters.shift();
+      lastMarker = markers.pop();
     }
 
     if (is(current, T.ListItemStart)) {
-      let delimiter = delimiters[0];
-
-      if (delimiter.value != null) {
-        result.push({
-          kind: current.kind,
-          value: `${delimiter.value}${delimiter.delimiter} `
-        });
-        delimiter.value++;
-      } else {
-        result.push({
-          kind: current.kind,
-          value: `${delimiter.delimiter} `
-        });
+      let indent = "";
+      let outerMarker = markers[markers.length - 2];
+      if (outerMarker) {
+        indent = " ".repeat(outerMarker.indent);
       }
-    } else if (is(current, T.ThematicBreak)) {
+
+      if (marker.value != null) {
+        let number = `${indent}${marker.value}${marker.delimiter} `;
+        result.push(T.ListItemStart(number));
+        marker.value++;
+        marker.indent = number.length;
+      } else {
+        let bullet = `${indent}${marker.delimiter} `;
+        result.push(T.ListItemStart(bullet));
+        marker.indent = bullet.length;
+      }
+    } else if (is(current, T.ThematicBreak) && is(previous, T.ListItemStart)) {
       // If you want a thematic break in a list item, use a different bullet:
       // https://spec.commonmark.org/0.29/#example-31
-      let delimiter = delimiters[0];
-      if (delimiter.delimiter === "*") {
+      if (marker.delimiter === "*") {
         result.push(T.ThematicBreak("-"));
+      }
+    } else if (
+      is(current, T.SoftLineBreak) ||
+      is(current, T.BlockSeparator) ||
+      is(current, T.HardLineBreak) ||
+      is(current, T.BlockquoteLineEnd)
+    ) {
+      result.push(current);
+      if (marker) {
+        result.push(" ".repeat(marker.indent));
       }
     } else {
       result.push(current);
--- packages/@atjson/renderer-commonmark/test/add-delimiters-to-lists-test.ts
+++ packages/@atjson/renderer-commonmark/test/add-delimiters-to-lists-test.ts
@@ -1,125 +1,205 @@
 import { addDelimitersToLists } from "../src/lib/add-delimiters-to-lists";
-import * as Tokens from "../src/lib/tokens";
+import * as T from "../src/lib/tokens";
 
 describe("addDelimitersToLists", () => {
   describe("numbered lists", () => {
     test("a single list", () => {
       let stream = [
-        Tokens.NumberedListStart(1),
-        Tokens.ListItemStart(""),
+        T.NumberedListStart(1),
+        T.ListItemStart(""),
         "one fish",
-        Tokens.ListItemEnd,
-        Tokens.ListItemStart(""),
+        T.ListItemEnd,
+        T.ListItemStart(""),
         "two fish",
-        Tokens.ListItemEnd,
-        Tokens.ListItemStart(""),
+        T.ListItemEnd,
+        T.ListItemStart(""),
         "red fish",
-        Tokens.ListItemEnd,
-        Tokens.ListItemStart(""),
+        T.ListItemEnd,
+        T.ListItemStart(""),
         "blue fish",
-        Tokens.ListItemEnd,
-        Tokens.NumberedListEnd
+        T.ListItemEnd,
+        T.NumberedListEnd
       ];
 
       expect(addDelimitersToLists(stream)).toEqual([
-        Tokens.NumberedListStart(1),
-        Tokens.ListItemStart("1. "),
+        T.NumberedListStart(1),
+        T.ListItemStart("1. "),
         "one fish",
-        Tokens.ListItemEnd,
-        Tokens.ListItemStart("2. "),
+        T.ListItemEnd,
+        T.ListItemStart("2. "),
         "two fish",
-        Tokens.ListItemEnd,
-        Tokens.ListItemStart("3. "),
+        T.ListItemEnd,
+        T.ListItemStart("3. "),
         "red fish",
-        Tokens.ListItemEnd,
-        Tokens.ListItemStart("4. "),
+        T.ListItemEnd,
+        T.ListItemStart("4. "),
         "blue fish",
-        Tokens.ListItemEnd,
-        Tokens.NumberedListEnd
+        T.ListItemEnd,
+        T.NumberedListEnd
       ]);
     });
 
     test("multiple adjacent lists", () => {
       let stream = [
-        Tokens.NumberedListStart(1),
-        Tokens.ListItemStart(""),
+        T.NumberedListStart(1),
+        T.ListItemStart(""),
         "one fish",
-        Tokens.ListItemEnd,
-        Tokens.NumberedListEnd,
-        Tokens.NumberedListStart(2),
-        Tokens.ListItemStart(""),
+        T.ListItemEnd,
+        T.NumberedListEnd,
+        T.NumberedListStart(2),
+        T.ListItemStart(""),
         "two fish",
-        Tokens.ListItemEnd,
-        Tokens.NumberedListEnd,
-        Tokens.NumberedListStart(3),
-        Tokens.ListItemStart(""),
+        T.ListItemEnd,
+        T.NumberedListEnd,
+        T.NumberedListStart(3),
+        T.ListItemStart(""),
         "red fish",
-        Tokens.ListItemEnd,
-        Tokens.NumberedListEnd,
-        Tokens.NumberedListStart(5),
-        Tokens.ListItemStart(""),
+        T.ListItemEnd,
+        T.NumberedListEnd,
+        T.NumberedListStart(5),
+        T.ListItemStart(""),
         "blue fish",
-        Tokens.ListItemEnd,
-        Tokens.NumberedListEnd
+        T.ListItemEnd,
+        T.NumberedListEnd
       ];
 
       expect(addDelimitersToLists(stream)).toEqual([
-        Tokens.NumberedListStart(1),
-        Tokens.ListItemStart("1. "),
+        T.NumberedListStart(1),
+        T.ListItemStart("1. "),
         "one fish",
-        Tokens.ListItemEnd,
-        Tokens.NumberedListEnd,
-        Tokens.NumberedListStart(2),
-        Tokens.ListItemStart("2) "),
+        T.ListItemEnd,
+        T.NumberedListEnd,
+        T.NumberedListStart(2),
+        T.ListItemStart("2) "),
         "two fish",
-        Tokens.ListItemEnd,
-        Tokens.NumberedListEnd,
-        Tokens.NumberedListStart(3),
-        Tokens.ListItemStart("3. "),
+        T.ListItemEnd,
+        T.NumberedListEnd,
+        T.NumberedListStart(3),
+        T.ListItemStart("3. "),
         "red fish",
-        Tokens.ListItemEnd,
-        Tokens.NumberedListEnd,
-        Tokens.NumberedListStart(5),
-        Tokens.ListItemStart("5) "),
+        T.ListItemEnd,
+        T.NumberedListEnd,
+        T.NumberedListStart(5),
+        T.ListItemStart("5) "),
         "blue fish",
-        Tokens.ListItemEnd,
-        Tokens.NumberedListEnd
+        T.ListItemEnd,
+        T.NumberedListEnd
       ]);
     });
 
     test("non-adjacent lists use the default markers", () => {
       let stream = [
-        Tokens.NumberedListStart(1),
-        Tokens.ListItemStart(""),
+        T.NumberedListStart(1),
+        T.ListItemStart(""),
         "one fish",
-        Tokens.ListItemEnd,
-        Tokens.ListItemStart(""),
+        T.ListItemEnd,
+        T.ListItemStart(""),
         "two fish",
-        Tokens.ListItemEnd,
-        Tokens.NumberedListEnd,
+        T.ListItemEnd,
+        T.NumberedListEnd,
         "red fish",
-        Tokens.NumberedListStart(3),
-        Tokens.ListItemStart(""),
+        T.NumberedListStart(3),
+        T.ListItemStart(""),
         "blue fish",
-        Tokens.ListItemEnd,
-        Tokens.NumberedListEnd
+        T.ListItemEnd,
+        T.NumberedListEnd
       ];
 
       expect(addDelimitersToLists(stream)).toEqual([
-        Tokens.NumberedListStart(1),
-        Tokens.ListItemStart("1. "),
+        T.NumberedListStart(1),
+        T.ListItemStart("1. "),
         "one fish",
-        Tokens.ListItemEnd,
-        Tokens.ListItemStart("2. "),
+        T.ListItemEnd,
+        T.ListItemStart("2. "),
         "two fish",
-        Tokens.ListItemEnd,
-        Tokens.NumberedListEnd,
+        T.ListItemEnd,
+        T.NumberedListEnd,
         "red fish",
-        Tokens.NumberedListStart(3),
-        Tokens.ListItemStart("3. "),
+        T.NumberedListStart(3),
+        T.ListItemStart("3. "),
         "blue fish",
-        Tokens.ListItemEnd,
-        Tokens.NumberedListEnd
+        T.ListItemEnd,
+        T.NumberedListEnd
+      ]);
+    });
+
+    test("sublists", () => {
+      let stream = [
+        T.NumberedListStart(1),
+        T.ListItemStart(""),
+        "one fish",
+        T.ListItemEnd,
+        T.NumberedListStart(2),
+        T.ListItemStart(""),
+        "two fish",
+        T.ListItemEnd,
+        T.NumberedListStart(3),
+        T.ListItemStart(""),
+        "red fish",
+        T.ListItemEnd,
+        T.ListItemStart(""),
+        "blue fish",
+        T.ListItemEnd,
+        T.NumberedListEnd,
+        T.NumberedListEnd,
+        T.NumberedListEnd
+      ];
+
+      expect(addDelimitersToLists(stream)).toEqual([
+        T.NumberedListStart(1),
+        T.ListItemStart("1. "),
+        "one fish",
+        T.ListItemEnd,
+        T.NumberedListStart(2),
+        T.ListItemStart("   2. "),
+        "two fish",
+        T.ListItemEnd,
+        T.NumberedListStart(3),
+        T.ListItemStart("      3. "),
+        "red fish",
+        T.ListItemEnd,
+        T.ListItemStart("      4. "),
+        "blue fish",
+        T.ListItemEnd,
+        T.NumberedListEnd,
+        T.NumberedListEnd,
+        T.NumberedListEnd
+      ]);
+    });
+
+    test("line breaks inside of lists", () => {
+      let stream = [
+        T.NumberedListStart(1),
+        T.ListItemStart(""),
+        "one fish",
+        T.SoftLineBreak,
+        "two fish",
+        T.NumberedListStart(22),
+        T.ListItemStart(""),
+        "red fish",
+        T.SoftLineBreak,
+        "blue fish",
+        T.ListItemEnd,
+        T.NumberedListEnd,
+        T.NumberedListEnd
+      ];
+
+      expect(addDelimitersToLists(stream)).toEqual([
+        T.NumberedListStart(1),
+        T.ListItemStart("1. "),
+        "one fish",
+        T.SoftLineBreak,
+        "   ",
+        "two fish",
+        T.NumberedListStart(22),
+        T.ListItemStart("   22. "),
+        "red fish",
+        T.SoftLineBreak,
+        "       ",
+        "blue fish",
+        T.ListItemEnd,
+        T.NumberedListEnd,
+        T.NumberedListEnd
       ]);
     });
   });
@@ -127,141 +207,220 @@ describe("addDelimitersToLists", () => {
   describe("bulleted lists", () => {
     test("a single list", () => {
       let stream = [
-        Tokens.BulletedListStart,
-        Tokens.ListItemStart(""),
+        T.BulletedListStart,
+        T.ListItemStart(""),
         "one fish",
-        Tokens.ListItemEnd,
-        Tokens.ListItemStart(""),
+        T.ListItemEnd,
+        T.ListItemStart(""),
         "two fish",
-        Tokens.ListItemEnd,
-        Tokens.ListItemStart(""),
+        T.ListItemEnd,
+        T.ListItemStart(""),
         "red fish",
-        Tokens.ListItemEnd,
-        Tokens.ListItemStart(""),
+        T.ListItemEnd,
+        T.ListItemStart(""),
         "blue fish",
-        Tokens.ListItemEnd,
-        Tokens.BulletedListEnd
+        T.ListItemEnd,
+        T.BulletedListEnd
       ];
 
       expect(addDelimitersToLists(stream)).toEqual([
-        Tokens.BulletedListStart,
-        Tokens.ListItemStart("- "),
+        T.BulletedListStart,
+        T.ListItemStart("- "),
         "one fish",
-        Tokens.ListItemEnd,
-        Tokens.ListItemStart("- "),
+        T.ListItemEnd,
+        T.ListItemStart("- "),
         "two fish",
-        Tokens.ListItemEnd,
-        Tokens.ListItemStart("- "),
+        T.ListItemEnd,
+        T.ListItemStart("- "),
         "red fish",
-        Tokens.ListItemEnd,
-        Tokens.ListItemStart("- "),
+        T.ListItemEnd,
+        T.ListItemStart("- "),
         "blue fish",
-        Tokens.ListItemEnd,
-        Tokens.BulletedListEnd
+        T.ListItemEnd,
+        T.BulletedListEnd
       ]);
     });
 
     test("multiple adjacent lists", () => {
       let stream = [
-        Tokens.BulletedListStart,
-        Tokens.ListItemStart(""),
+        T.BulletedListStart,
+        T.ListItemStart(""),
         "one fish",
-        Tokens.ListItemEnd,
-        Tokens.BulletedListEnd,
-        Tokens.BulletedListStart,
-        Tokens.ListItemStart(""),
+        T.ListItemEnd,
+        T.BulletedListEnd,
+        T.BulletedListStart,
+        T.ListItemStart(""),
         "two fish",
-        Tokens.ListItemEnd,
-        Tokens.ListItemStart(""),
+        T.ListItemEnd,
+        T.ListItemStart(""),
         "red fish",
-        Tokens.ListItemEnd,
-        Tokens.BulletedListEnd,
-        Tokens.BulletedListStart,
-        Tokens.ListItemStart(""),
+        T.ListItemEnd,
+        T.BulletedListEnd,
+        T.BulletedListStart,
+        T.ListItemStart(""),
         "blue fish",
-        Tokens.ListItemEnd,
-        Tokens.BulletedListEnd
+        T.ListItemEnd,
+        T.BulletedListEnd
       ];
 
       expect(addDelimitersToLists(stream)).toEqual([
-        Tokens.BulletedListStart,
-        Tokens.ListItemStart("- "),
+        T.BulletedListStart,
+        T.ListItemStart("- "),
         "one fish",
-        Tokens.ListItemEnd,
-        Tokens.BulletedListEnd,
-        Tokens.BulletedListStart,
-        Tokens.ListItemStart("* "),
+        T.ListItemEnd,
+        T.BulletedListEnd,
+        T.BulletedListStart,
+        T.ListItemStart("* "),
         "two fish",
-        Tokens.ListItemEnd,
-        Tokens.ListItemStart("* "),
+        T.ListItemEnd,
+        T.ListItemStart("* "),
         "red fish",
-        Tokens.ListItemEnd,
-        Tokens.BulletedListEnd,
-        Tokens.BulletedListStart,
-        Tokens.ListItemStart("- "),
+        T.ListItemEnd,
+        T.BulletedListEnd,
+        T.BulletedListStart,
+        T.ListItemStart("- "),
         "blue fish",
-        Tokens.ListItemEnd,
-        Tokens.BulletedListEnd
+        T.ListItemEnd,
+        T.BulletedListEnd
+      ]);
+    });
+
+    test("sublists", () => {
+      let stream = [
+        T.BulletedListStart,
+        T.ListItemStart(""),
+        "one fish",
+        T.ListItemEnd,
+        T.BulletedListStart,
+        T.ListItemStart(""),
+        "two fish",
+        T.ListItemEnd,
+        T.ListItemStart(""),
+        "red fish",
+        T.ListItemEnd,
+        T.BulletedListStart,
+        T.ListItemStart(""),
+        "blue fish",
+        T.ListItemEnd,
+        T.BulletedListEnd,
+        T.BulletedListEnd,
+        T.BulletedListEnd
+      ];
+
+      expect(addDelimitersToLists(stream)).toEqual([
+        T.BulletedListStart,
+        T.ListItemStart("- "),
+        "one fish",
+        T.ListItemEnd,
+        T.BulletedListStart,
+        T.ListItemStart("  - "),
+        "two fish",
+        T.ListItemEnd,
+        T.ListItemStart("  - "),
+        "red fish",
+        T.ListItemEnd,
+        T.BulletedListStart,
+        T.ListItemStart("    - "),
+        "blue fish",
+        T.ListItemEnd,
+        T.BulletedListEnd,
+        T.BulletedListEnd,
+        T.BulletedListEnd
       ]);
     });
 
     test("non-adjacent lists use the default markers", () => {
       let stream = [
-        Tokens.BulletedListStart,
-        Tokens.ListItemStart(""),
+        T.BulletedListStart,
+        T.ListItemStart(""),
         "one fish",
-        Tokens.ListItemEnd,
-        Tokens.BulletedListEnd,
+        T.ListItemEnd,
+        T.BulletedListEnd,
         "two fish",
+        T.SoftLineBreak,
         "red fish",
-        Tokens.BulletedListStart,
-        Tokens.ListItemStart(""),
+        T.BulletedListStart,
+        T.ListItemStart(""),
         "blue fish",
-        Tokens.ListItemEnd,
-        Tokens.BulletedListEnd
+        T.ListItemEnd,
+        T.BulletedListEnd
       ];
 
       expect(addDelimitersToLists(stream)).toEqual([
-        Tokens.BulletedListStart,
-        Tokens.ListItemStart("- "),
+        T.BulletedListStart,
+        T.ListItemStart("- "),
         "one fish",
-        Tokens.ListItemEnd,
-        Tokens.BulletedListEnd,
+        T.ListItemEnd,
+        T.BulletedListEnd,
         "two fish",
+        T.SoftLineBreak,
         "red fish",
-        Tokens.BulletedListStart,
-        Tokens.ListItemStart("- "),
+        T.BulletedListStart,
+        T.ListItemStart("- "),
         "blue fish",
-        Tokens.ListItemEnd,
-        Tokens.BulletedListEnd
+        T.ListItemEnd,
+        T.BulletedListEnd
+      ]);
+    });
+
+    test("soft line breaks inside of lists", () => {
+      let stream = [
+        T.BulletedListStart,
+        T.ListItemStart(""),
+        "one fish",
+        T.SoftLineBreak,
+        "two fish",
+        T.SoftLineBreak,
+        "red fish",
+        T.SoftLineBreak,
+        "blue fish",
+        T.ListItemEnd,
+        T.BulletedListEnd
+      ];
+
+      expect(addDelimitersToLists(stream)).toEqual([
+        T.BulletedListStart,
+        T.ListItemStart("- "),
+        "one fish",
+        T.SoftLineBreak,
+        "  ",
+        "two fish",
+        T.SoftLineBreak,
+        "  ",
+        "red fish",
+        T.SoftLineBreak,
+        "  ",
+        "blue fish",
+        T.ListItemEnd,
+        T.BulletedListEnd
       ]);
     });
 
     test("thematic breaks inside of lists", () => {
       let stream = [
-        Tokens.BulletedListStart,
-        Tokens.ListItemStart(""),
+        T.BulletedListStart,
+        T.ListItemStart(""),
         "one fish",
-        Tokens.ListItemEnd,
-        Tokens.BulletedListEnd,
-        Tokens.BulletedListStart,
-        Tokens.ListItemStart(""),
-        Tokens.ThematicBreak("*"),
-        Tokens.ListItemEnd,
-        Tokens.BulletedListEnd
+        T.ListItemEnd,
+        T.BulletedListEnd,
+        T.BulletedListStart,
+        T.ListItemStart(""),
+        T.ThematicBreak("*"),
+        T.ListItemEnd,
+        T.BulletedListEnd
       ];
 
       expect(addDelimitersToLists(stream)).toEqual([
-        Tokens.BulletedListStart,
-        Tokens.ListItemStart("- "),
+        T.BulletedListStart,
+        T.ListItemStart("- "),
         "one fish",
-        Tokens.ListItemEnd,
-        Tokens.BulletedListEnd,
-        Tokens.BulletedListStart,
-        Tokens.ListItemStart("* "),
-        Tokens.ThematicBreak("-"),
-        Tokens.ListItemEnd,
-        Tokens.BulletedListEnd
+        T.ListItemEnd,
+        T.BulletedListEnd,
+        T.BulletedListStart,
+        T.ListItemStart("* "),
+        T.ThematicBreak("-"),
+        T.ListItemEnd,
+        T.BulletedListEnd
       ]);
     });
   });
