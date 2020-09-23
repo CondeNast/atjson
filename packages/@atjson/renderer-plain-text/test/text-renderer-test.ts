1cb452ba3f7b7ec29112ece2ca8df077e72efae7
leahzxxz <gryffinzhao@gmail.com>
2020-09-23 15:41:45 -0400

    feat: add annotations to plain text renderer

--- packages/@atjson/renderer-plain-text/test/text-renderer-test.ts
+++ packages/@atjson/renderer-plain-text/test/text-renderer-test.ts
@@ -1,6 +1,11 @@
-import Document from "@atjson/document";
+import Document, { ParseAnnotation } from "@atjson/document";
 import HTMLSource from "@atjson/source-html";
-import OffsetSource from "@atjson/offset-annotations";
+import OffsetSource, {
+  LineBreak,
+  List,
+  ListItem,
+  Pullquote,
+} from "@atjson/offset-annotations";
 
 import PlainTextRenderer from "../src";
 
@@ -38,20 +43,179 @@ describe("PlainTextRenderer", () => {
     expect(text).toBe("This is some fancy text.");
   });
 
+  it("adds newlines between paragraphs", () => {
+    let doc = HTMLSource.fromRaw("<p>One fish</p><p>Two fish</p>").convertTo(
+      OffsetSource
+    );
+
+    let text = PlainTextRenderer.render(doc);
+    expect(text).toBe("One fish\n\nTwo fish\n\n");
+  });
+
+  it("headings have newlines added around them", () => {
+    let doc = HTMLSource.fromRaw("<h2>One fish</h2><p>Two fish</p>").convertTo(
+      OffsetSource
+    );
+
+    let text = PlainTextRenderer.render(doc);
+    expect(text).toBe("One fish\n\nTwo fish\n\n");
+  });
+
+  it("blockquotes have newlines added around them", () => {
+    let doc = HTMLSource.fromRaw(
+      "<blockquote>One fish</blockquote><p>Two fish</p>"
+    ).convertTo(OffsetSource);
+
+    let text = PlainTextRenderer.render(doc);
+    expect(text).toBe("One fish\n\nTwo fish\n\n");
+  });
+
+  it("pullquotes have newlines added around them", () => {
+    let doc = new OffsetSource({
+      content: "One fish\uFFFCTwo fish",
+      annotations: [
+        new Pullquote({
+          start: 8,
+          end: 9,
+        }),
+      ],
+    });
+
+    let text = PlainTextRenderer.render(doc);
+    expect(text).toBe("One fish\n\nTwo fish");
+  });
+
   it("renders line breaks", () => {
     let document = new OffsetSource({
-      content: "first linesecond line",
+      content: "first line\uFFFCsecond line",
       annotations: [
-        {
-          type: "-offset-line-break",
+        new LineBreak({
           start: 10,
-          end: 12,
-          attributes: {},
-        },
+          end: 11,
+        }),
       ],
     });
 
     let text = PlainTextRenderer.render(document);
     expect(text).toBe("first line\nsecond line");
+  });
+
+  test.each([undefined, 2])(
+    "numbered lists starting at %s are pretty printed",
+    (startsAt) => {
+      let document = new OffsetSource({
+        content: "one fish\ntwo fish\nred fish\nblue fish",
+        annotations: [
+          new List({
+            start: 0,
+            end: 36,
+            attributes: {
+              type: "numbered",
+              startsAt,
+            },
+          }),
+          new ListItem({
+            start: 0,
+            end: 8,
+          }),
+          new ParseAnnotation({
+            start: 8,
+            end: 9,
+            attributes: {
+              reason: "list item",
+            },
+          }),
+          new ListItem({
+            start: 9,
+            end: 17,
+          }),
+          new ParseAnnotation({
+            start: 17,
+            end: 18,
+            attributes: {
+              reason: "list item",
+            },
+          }),
+          new ListItem({
+            start: 18,
+            end: 26,
+          }),
+          new ParseAnnotation({
+            start: 26,
+            end: 27,
+            attributes: {
+              reason: "list item",
+            },
+          }),
+          new ListItem({
+            start: 27,
+            end: 36,
+          }),
+        ],
+      });
+
+      let index = startsAt ?? 1;
+      let text = PlainTextRenderer.render(document);
+      expect(text).toBe(
+        `${index}. one fish\n${index + 1}. two fish\n${index + 2}. red fish\n${
+          index + 3
+        }. blue fish\n`
+      );
+    }
+  );
+
+  test("bulleted lists are pretty printed", () => {
+    let document = new OffsetSource({
+      content: "one fish\ntwo fish\nred fish\nblue fish",
+      annotations: [
+        new List({
+          start: 0,
+          end: 36,
+          attributes: {
+            type: "bulleted",
+          },
+        }),
+        new ListItem({
+          start: 0,
+          end: 8,
+        }),
+        new ParseAnnotation({
+          start: 8,
+          end: 9,
+          attributes: {
+            reason: "list item",
+          },
+        }),
+        new ListItem({
+          start: 9,
+          end: 17,
+        }),
+        new ParseAnnotation({
+          start: 17,
+          end: 18,
+          attributes: {
+            reason: "list item",
+          },
+        }),
+        new ListItem({
+          start: 18,
+          end: 26,
+        }),
+        new ParseAnnotation({
+          start: 26,
+          end: 27,
+          attributes: {
+            reason: "list item",
+          },
+        }),
+        new ListItem({
+          start: 27,
+          end: 36,
+        }),
+      ],
+    });
+
+    let text = PlainTextRenderer.render(document);
+    expect(text).toBe("- one fish\n- two fish\n- red fish\n- blue fish\n");
   });
 });
