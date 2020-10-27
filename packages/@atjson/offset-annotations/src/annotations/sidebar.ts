2dc97671f77b4332319e6829312cd26b03fb6781
dima <dkorenblyum@gmail.com>
2020-10-27 14:04:37 -0400

    fix: add comment for sidebar

--- packages/@atjson/offset-annotations/src/annotations/sidebar.ts
+++ packages/@atjson/offset-annotations/src/annotations/sidebar.ts
@@ -1,5 +1,10 @@
 import { BlockAnnotation } from "@atjson/document";
 
+/**
+ * Sidebar is a callout whose contents should be placed adjacent to
+ * the body, graphically separate but with contextual connection.
+ **/
+
 export class Sidebar extends BlockAnnotation<{
   inset?: "left" | "right";
 }> {
