a59a90b119daaf01b4874ab0bba7cbec670794cd
Rahul Radhakrishnan <rahul_radhakrishnan@condenast.com>
2021-08-16 22:18:05 +0530

    feat(link): updated link style as union types

--- packages/@atjson/offset-annotations/src/annotations/link.ts
+++ packages/@atjson/offset-annotations/src/annotations/link.ts
@@ -6,7 +6,7 @@ export class Link extends InlineAnnotation<{
   rel?: string;
   target?: string;
   isAffiliateLink?: boolean;
-  linkStyle?: string;
+  linkStyle?: "button";
 }> {
   static vendorPrefix = "offset";
   static type = "link";
