2894f1a556a10f90fce272d68953d99bb069bca3
anurag-cn <anurag_kumar@condenast.com>
2021-09-06 16:49:46 +0530

    fix: adding size attribute to ceros embed offset annotations

--- packages/@atjson/offset-annotations/src/annotations/ceros-embed.ts
+++ packages/@atjson/offset-annotations/src/annotations/ceros-embed.ts
@@ -4,7 +4,7 @@ export class CerosEmbed extends ObjectAnnotation<{
   url: string;
   aspectRatio: number;
   mobileAspectRatio?: number;
-  size: "small" | "medium" | "large";
+  size?: "small" | "medium" | "large";
   /**
    * A named identifier used to quickly jump to this item
    */
