c5039cc980c0f41dd2ad556df9bfc2472bb33889
Tim Evans <tim.c.evans@me.com>
2021-11-29 13:44:00 -0500

    fix: escape blockquote characters

--- packages/@atjson/renderer-commonmark/src/index.ts
+++ packages/@atjson/renderer-commonmark/src/index.ts
@@ -218,6 +218,7 @@ function escapePunctuation(
   let escaped = text
     .replace(/([#!*+=\\^_`{|}~])/g, "\\$1")
     .replace(/(\[)([^\]]*$)/g, "\\$1$2") // Escape bare opening brackets [
+    .replace(/(^[\s]*)>/g, "$1\\>") // Escape >
     .replace(/(^[^[]*)(\].*$)/g, "$1\\$2") // Escape bare closing brackets ]
     .replace(/(\]\()/g, "]\\(") // Escape parenthesis ](
     .replace(/^(\s*\d+)\.(\s+)/gm, "$1\\.$2") // Escape list items; not all numbers
