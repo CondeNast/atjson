d3aef554cc950b87f02f854b888e4c83e070ca0b
Tim Evans <tim.c.evans@me.com>
2022-12-08 11:25:22 -0500

    chore: add HTML fixtures

--- perf-kit/fixtures/index.ts
+++ perf-kit/fixtures/index.ts
@@ -4,3 +4,7 @@ import { join } from "path";
 export const md = readdirSync(__dirname)
   .filter((filename) => filename.endsWith(".md"))
   .map((filename) => readFileSync(join(__dirname, filename)).toString());
+
+export const html = readdirSync(__dirname)
+  .filter((filename) => filename.endsWith(".html"))
+  .map((filename) => readFileSync(join(__dirname, filename)).toString());
