b60646cee25c979c84e92ce67847ec7d1966aa35
Tim Evans <tim.c.evans@me.com>
2023-04-06 14:34:24 -0400

    perf: use .update instead of set / rename / unset
    serializing annotations to json
    was causing performance
    deoptimizations and
    a fair amount of garbage
    collection to be triggered
    on larger documents

--- packages/@atjson/source-commonmark/src/converter.ts
+++ packages/@atjson/source-commonmark/src/converter.ts
@@ -1,27 +1,100 @@
-import OffsetSource, { CodeBlock, Image } from "@atjson/offset-annotations";
+import OffsetSource, {
+  Blockquote,
+  Bold,
+  Code,
+  CodeBlock,
+  HTML,
+  Heading,
+  HorizontalRule,
+  Image,
+  Italic,
+  LineBreak,
+  Link,
+  List,
+  ListItem,
+  Paragraph,
+} from "@atjson/offset-annotations";
 import CommonmarkSource from "./source";
+import {
+  BulletList as CommonmarkBulletList,
+  Heading as CommonmarkHeading,
+  Image as CommonmarkImage,
+  Link as CommonmarkLink,
+  ListItem as CommonmarkListItem,
+  OrderedList as CommonmarkOrderedList,
+} from "./annotations";
 
 CommonmarkSource.defineConverterTo(
   OffsetSource,
   function commonmarkToOffset(doc) {
-    doc
-      .where({ type: "-commonmark-blockquote" })
-      .set({ type: "-offset-blockquote" });
+    doc.where({ type: "-commonmark-blockquote" }).update((blockquote) => {
+      doc.replaceAnnotation(
+        blockquote,
+        new Blockquote({
+          id: blockquote.id,
+          start: blockquote.start,
+          end: blockquote.end,
+        })
+      );
+    });
+
     doc
       .where({ type: "-commonmark-bullet_list" })
-      .set({ type: "-offset-list", attributes: { "-offset-type": "bulleted" } })
-      .rename({ attributes: { "-commonmark-loose": "-offset-loose" } });
-    doc.where({ type: "-commonmark-code_block" }).set({
-      type: "-offset-code-block",
+      .update((list: CommonmarkBulletList) => {
+        doc.replaceAnnotation(
+          list,
+          new List({
+            id: list.id,
+            start: list.start,
+            end: list.end,
+            attributes: {
+              type: "bulleted",
+              loose: list.attributes.loose,
+            },
+          })
+        );
+      });
+
+    doc.where({ type: "-commonmark-code_block" }).update((code) => {
+      doc.replaceAnnotation(
+        code,
+        new CodeBlock({
+          id: code.id,
+          start: code.start,
+          end: code.end,
+          attributes: {},
+        })
+      );
     });
-    doc
-      .where({ type: "-commonmark-code_inline" })
-      .set({ type: "-offset-code", attributes: { "-offset-style": "inline" } });
-    doc.where({ type: "-commonmark-em" }).set({ type: "-offset-italic" });
+
+    doc.where({ type: "-commonmark-code_inline" }).update((code) => {
+      doc.replaceAnnotation(
+        code,
+        new Code({
+          id: code.id,
+          start: code.start,
+          end: code.end,
+        })
+      );
+    });
+
+    doc.where({ type: "-commonmark-em" }).update((emphasis) => {
+      doc.replaceAnnotation(
+        emphasis,
+        new Italic({
+          id: emphasis.id,
+          start: emphasis.start,
+          end: emphasis.end,
+          attributes: emphasis.attributes,
+        })
+      );
+    });
+
     doc.where({ type: "-commonmark-fence" }).update((fence) => {
       doc.replaceAnnotation(
         fence,
         new CodeBlock({
+          id: fence.id,
           start: fence.start,
           end: fence.end,
           attributes: {
@@ -31,63 +104,163 @@ CommonmarkSource.defineConverterTo(
       );
     });
 
-    doc
-      .where({ type: "-commonmark-hardbreak" })
-      .set({ type: "-offset-line-break" });
+    doc.where({ type: "-commonmark-hardbreak" }).update((linebreak) => {
+      doc.replaceAnnotation(
+        linebreak,
+        new LineBreak({
+          id: linebreak.id,
+          start: linebreak.start,
+          end: linebreak.end,
+        })
+      );
+    });
+
     doc
       .where({ type: "-commonmark-heading" })
-      .set({ type: "-offset-heading" })
-      .rename({ attributes: { "-commonmark-level": "-offset-level" } });
-    doc
-      .where({ type: "-commonmark-hr" })
-      .set({ type: "-offset-horizontal-rule" });
-    doc
-      .where({ type: "-commonmark-html_block" })
-      .set({ type: "-offset-html", attributes: { "-offset-style": "block" } });
-    doc
-      .where({ type: "-commonmark-html_inline" })
-      .set({ type: "-offset-html", attributes: { "-offset-style": "inline" } });
+      .update((heading: CommonmarkHeading) => {
+        let { level, ...attributes } = heading.attributes;
+        doc.replaceAnnotation(
+          heading,
+          new Heading({
+            id: heading.id,
+            start: heading.start,
+            end: heading.end,
+            attributes: {
+              level: Math.min(Math.max(level, 1), 6) as 1 | 2 | 3 | 4 | 5 | 6,
+              ...attributes,
+            },
+          })
+        );
+      });
 
-    doc.where({ type: "-commonmark-image" }).update((image) => {
+    doc.where({ type: "-commonmark-hr" }).update((rule) => {
       doc.replaceAnnotation(
-        image,
-        new Image({
-          start: image.start,
-          end: image.end,
+        rule,
+        new HorizontalRule({
+          id: rule.id,
+          start: rule.start,
+          end: rule.end,
+        })
+      );
+    });
+
+    doc.where({ type: "-commonmark-html_block" }).update((html) => {
+      doc.replaceAnnotation(
+        html,
+        new HTML({
+          id: html.id,
+          start: html.start,
+          end: html.end,
           attributes: {
-            url: image.attributes.src,
-            title: image.attributes.title,
-            description: image.attributes.alt,
+            style: "block",
+          },
+        })
+      );
+    });
+
+    doc.where({ type: "-commonmark-html_inline" }).update((html) => {
+      doc.replaceAnnotation(
+        html,
+        new HTML({
+          id: html.id,
+          start: html.start,
+          end: html.end,
+          attributes: {
+            style: "inline",
           },
         })
       );
     });
 
     doc
-      .where({ type: "-commonmark-link" })
-      .set({ type: "-offset-link" })
-      .rename({
-        attributes: {
-          "-commonmark-href": "-offset-url",
-          "-commonmark-title": "-offset-title",
-        },
+      .where({ type: "-commonmark-image" })
+      .update((image: CommonmarkImage) => {
+        doc.replaceAnnotation(
+          image,
+          new Image({
+            id: image.id,
+            start: image.start,
+            end: image.end,
+            attributes: {
+              url: image.attributes.src,
+              title: image.attributes.title,
+              description: image.attributes.alt,
+            },
+          })
+        );
       });
+
+    doc.where({ type: "-commonmark-link" }).update((link: CommonmarkLink) => {
+      let { href, title, ...attributes } = link.attributes;
+      doc.replaceAnnotation(
+        link,
+        new Link({
+          id: link.id,
+          start: link.start,
+          end: link.end,
+          attributes: {
+            url: href,
+            title,
+            ...attributes,
+          },
+        })
+      );
+    });
+
     doc
       .where({ type: "-commonmark-list_item" })
-      .set({ type: "-offset-list-item" });
+      .update((listItem: CommonmarkListItem) => {
+        doc.replaceAnnotation(
+          listItem,
+          new ListItem({
+            id: listItem.id,
+            start: listItem.start,
+            end: listItem.end,
+            attributes: listItem.attributes,
+          })
+        );
+      });
+
     doc
       .where({ type: "-commonmark-ordered_list" })
-      .set({ type: "-offset-list", attributes: { "-offset-type": "numbered" } })
-      .rename({
-        attributes: {
-          "-commonmark-start": "-offset-startsAt",
-          "-commonmark-loose": "-offset-loose",
-        },
+      .update((list: CommonmarkOrderedList) => {
+        doc.replaceAnnotation(
+          list,
+          new List({
+            id: list.id,
+            start: list.start,
+            end: list.end,
+            attributes: {
+              type: "numbered",
+              startsAt: list.attributes.start,
+              loose: list.attributes.loose,
+            },
+          })
+        );
       });
-    doc
-      .where({ type: "-commonmark-paragraph" })
-      .set({ type: "-offset-paragraph" });
-    doc.where({ type: "-commonmark-strong" }).set({ type: "-offset-bold" });
+
+    doc.where({ type: "-commonmark-paragraph" }).update((paragraph) => {
+      doc.replaceAnnotation(
+        paragraph,
+        new Paragraph({
+          id: paragraph.id,
+          start: paragraph.start,
+          end: paragraph.end,
+          attributes: paragraph.attributes,
+        })
+      );
+    });
+
+    doc.where({ type: "-commonmark-strong" }).update((strong) => {
+      doc.replaceAnnotation(
+        strong,
+        new Bold({
+          id: strong.id,
+          start: strong.start,
+          end: strong.end,
+        })
+      );
+    });
 
     return doc;
   }
--- packages/@atjson/source-html/src/converter/index.ts
+++ packages/@atjson/source-html/src/converter/index.ts
@@ -1,10 +1,23 @@
 import Document, { Annotation } from "@atjson/document";
 import OffsetSource, {
+  Blockquote,
+  Bold,
+  Code,
   CodeBlock,
-  List,
+  HorizontalRule,
   IframeEmbed,
+  Italic,
+  LineBreak,
+  Link,
+  List,
+  Section,
+  SmallCaps,
+  Strikethrough,
+  Subscript,
+  Superscript,
+  Underline,
 } from "@atjson/offset-annotations";
-import { OrderedList } from "../annotations";
+import { Anchor, OrderedList } from "../annotations";
 import HTMLSource from "../source";
 import convertSocialEmbeds from "./social-embeds";
 import convertThirdPartyEmbeds from "./third-party-embeds";
@@ -59,32 +72,60 @@ HTMLSource.defineConverterTo(OffsetSource, function HTMLToOffset(doc) {
     );
   });
 
-  doc
-    .where({ type: "-html-a" })
-    .set({ type: "-offset-link" })
-    .rename({
-      attributes: {
-        "-html-href": "-offset-url",
-        "-html-rel": "-offset-rel",
-        "-html-target": "-offset-target",
-        "-html-title": "-offset-title",
-      },
-    });
+  doc.where({ type: "-html-a" }).update((a: Anchor) => {
+    doc.replaceAnnotation(
+      a,
+      new Link({
+        id: a.id,
+        start: a.start,
+        end: a.end,
+        attributes: {
+          url: a.attributes.href ?? "",
+          rel: a.attributes.rel,
+          target: a.attributes.target,
+          title: a.attributes.title,
+        },
+      })
+    );
+  });
 
-  doc
-    .where({ type: "-html-blockquote" })
-    .set({ type: "-offset-blockquote" })
-    .rename({
-      attributes: {
-        "-html-id": "-offset-anchorName",
-      },
-    });
+  doc.where({ type: "-html-blockquote" }).update((blockquote) => {
+    doc.replaceAnnotation(
+      blockquote,
+      new Blockquote({
+        id: blockquote.id,
+        start: blockquote.start,
+        end: blockquote.end,
+        attributes: {
+          anchorName: blockquote.attributes.id,
+        },
+      })
+    );
+  });
 
   convertHeadings(doc);
   convertParagraphs(doc);
 
-  doc.where({ type: "-html-br" }).set({ type: "-offset-line-break" });
-  doc.where({ type: "-html-hr" }).set({ type: "-offset-horizontal-rule" });
+  doc.where({ type: "-html-br" }).update((lineBreak) => {
+    doc.replaceAnnotation(
+      lineBreak,
+      new LineBreak({
+        id: lineBreak.id,
+        start: lineBreak.start,
+        end: lineBreak.end,
+      })
+    );
+  });
+  doc.where({ type: "-html-hr" }).update((horizontalRule) => {
+    doc.replaceAnnotation(
+      horizontalRule,
+      new HorizontalRule({
+        id: horizontalRule.id,
+        start: horizontalRule.start,
+        end: horizontalRule.end,
+      })
+    );
+  });
 
   doc
     .where({ type: "-html-ul" })
@@ -114,15 +155,75 @@ HTMLSource.defineConverterTo(OffsetSource, function HTMLToOffset(doc) {
       },
     });
 
-  doc.where({ type: "-html-em" }).set({ type: "-offset-italic" });
-  doc.where({ type: "-html-i" }).set({ type: "-offset-italic" });
-  doc.where({ type: "-html-strong" }).set({ type: "-offset-bold" });
-  doc.where({ type: "-html-b" }).set({ type: "-offset-bold" });
-  doc.where({ type: "-html-del" }).set({ type: "-offset-strikethrough" });
-  doc.where({ type: "-html-s" }).set({ type: "-offset-strikethrough" });
-  doc.where({ type: "-html-sub" }).set({ type: "-offset-subscript" });
-  doc.where({ type: "-html-sup" }).set({ type: "-offset-superscript" });
-  doc.where({ type: "-html-u" }).set({ type: "-offset-underline" });
+  doc
+    .where((a) => a.type === "em" || a.type === "i")
+    .update((italic) => {
+      doc.replaceAnnotation(
+        italic,
+        new Italic({
+          id: italic.id,
+          start: italic.start,
+          end: italic.end,
+        })
+      );
+    });
+
+  doc
+    .where((a) => a.type === "strong" || a.type === "b")
+    .update((bold) => {
+      doc.replaceAnnotation(
+        bold,
+        new Bold({
+          id: bold.id,
+          start: bold.start,
+          end: bold.end,
+        })
+      );
+    });
+
+  doc
+    .where((a) => a.type === "del" || a.type === "s")
+    .update((strikethrough) => {
+      doc.replaceAnnotation(
+        strikethrough,
+        new Strikethrough({
+          id: strikethrough.id,
+          start: strikethrough.start,
+          end: strikethrough.end,
+        })
+      );
+    });
+
+  doc.where({ type: "-html-sub" }).update((subscript) => {
+    doc.replaceAnnotation(
+      subscript,
+      new Subscript({
+        id: subscript.id,
+        start: subscript.start,
+        end: subscript.end,
+      })
+    );
+  });
+  doc.where({ type: "-html-sup" }).update((superscript) => {
+    doc.replaceAnnotation(
+      superscript,
+      new Superscript({
+        id: superscript.id,
+        start: superscript.start,
+        end: superscript.end,
+      })
+    );
+  });
+  doc.where({ type: "-html-u" }).update((underline) => {
+    doc.replaceAnnotation(
+      underline,
+      new Underline({
+        id: underline.id,
+        start: underline.start,
+        end: underline.end,
+      })
+    );
+  });
 
   doc
     .where({ type: "-html-img" })
@@ -162,13 +263,38 @@ HTMLSource.defineConverterTo(OffsetSource, function HTMLToOffset(doc) {
       doc.removeAnnotation(pre);
     });
 
-  doc
-    .where({ type: "-html-code" })
-    .set({ type: "-offset-code", attributes: {} });
+  doc.where({ type: "-html-code" }).update((code) => {
+    doc.replaceAnnotation(
+      code,
+      new Code({
+        id: code.id,
+        start: code.start,
+        end: code.end,
+      })
+    );
+  });
 
-  doc.where({ type: "-html-section" }).set({ type: "-offset-section" });
+  doc.where({ type: "-html-section" }).update((section) => {
+    doc.replaceAnnotation(
+      section,
+      new Section({
+        id: section.id,
+        start: section.start,
+        end: section.end,
+      })
+    );
+  });
 
-  doc.where(isSmallCaps).set({ type: "-offset-small-caps", attributes: {} });
+  doc.where(isSmallCaps).update((smallCaps) => {
+    doc.replaceAnnotation(
+      smallCaps,
+      new SmallCaps({
+        id: smallCaps.id,
+        start: smallCaps.start,
+        end: smallCaps.end,
+      })
+    );
+  });
 
   return doc;
 });
