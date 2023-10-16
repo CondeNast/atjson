dcd070bc70ad6a4380534833f57b41622f03537a
Tim Evans <tim.c.evans@me.com>
2023-10-16 10:58:28 -0400

    chore: add layout and comments to cne-audio-embed

--- packages/@atjson/offset-annotations/src/annotations/cne-audio-embed.ts
+++ packages/@atjson/offset-annotations/src/annotations/cne-audio-embed.ts
@@ -6,9 +6,31 @@ export enum AudioEnvironments {
 }
 
 export class CneAudioEmbed extends BlockAnnotation<{
+  /**
+   * Indicates the environment that the audio was published in.
+   */
   audioEnv: AudioEnvironments;
+
+  /**
+   * The type of audio embed.
+   */
   audioType: string;
+
+  /**
+   * The unique id of the audio, used to embed the
+   * podcast / episode / etc.
+   */
   audioId: string;
+
+  /**
+   * Layout information, used to indicate mutually
+   * exclusive layouts, for example sizes, floats, etc.
+   */
+  layout?: string;
+
+  /**
+   * A named identifier used to quickly jump to this item
+   */
   anchorName?: string;
 }> {
   static vendorPrefix = "offset";
