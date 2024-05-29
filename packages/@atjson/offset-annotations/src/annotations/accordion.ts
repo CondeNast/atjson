6d7398d7da516e6ad276a52027d5a503c01ec1d2
Prabhakaran <vinay_prabhakaran@condenast.com>
2024-05-29 10:42:13 +0530

    feat: annotation updates

--- packages/@atjson/offset-annotations/src/annotations/accordion.ts
+++ packages/@atjson/offset-annotations/src/annotations/accordion.ts
@@ -2,14 +2,31 @@ import { BlockAnnotation } from "@atjson/document";
 
 export class Accordion extends BlockAnnotation<{
   /**
-   * A named identifier used to store the title of the
-   * content in the panel
+   * Slice referring to the header of the accordion,
+   * which is initially visible.
    */
   header: string;
   /**
-   * A named identifier used to store the content
+   * Slice referring to the panel content of the accordion.
+   * This should be shown when the panel is in a default
+   * opened state or it is opened by a user by tapping
+   * the header.
    */
-  panel: string;
+  panel?: string;
+  /**
+   * Initial state of this accordion.
+   * (TBD what consumers initially should do.)
+   */
+  opened?: boolean;
+  /**
+   * A named identifier used to represent the layout
+   * like full page width or half
+   */
+  layout?: string;
+  /**
+   * A named identifier used to quickly jump to this item
+   */
+  anchorName?: string;
 }> {
   static type = "accordion";
   static vendorPrefix = "offset";
