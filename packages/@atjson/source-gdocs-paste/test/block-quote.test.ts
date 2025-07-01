5e0db113c8fb6af2c9946d36e320e46e4a6c0cd2
Tim Evans <tim.c.evans@me.com>
2025-07-01 12:06:16 -0400

    chore: use stable ids for block-quote test

--- packages/@atjson/source-gdocs-paste/test/block-quote.test.ts
+++ packages/@atjson/source-gdocs-paste/test/block-quote.test.ts
@@ -14,47 +14,47 @@ describe("@atjson/source-gdocs-paste blockquotes", () => {
   });
 
   test("Indenting a paragraph makes a block quote", () => {
-    expect(serialize(doc)).toMatchInlineSnapshot(`
+    expect(serialize(doc, { withStableIds: true })).toMatchInlineSnapshot(`
       {
         "blocks": [
           {
             "attributes": {},
-            "id": "f7cd3075-8e6c-4eeb-919f-7c03467c84bb",
+            "id": "B00000000",
             "parents": [],
             "selfClosing": false,
             "type": "paragraph",
           },
           {
             "attributes": {},
-            "id": "f41117e5-7fa4-43f2-bff1-163347873ef9",
+            "id": "B00000001",
             "parents": [],
             "selfClosing": false,
             "type": "blockquote",
           },
           {
             "attributes": {},
-            "id": "20c97926-897b-47f2-a6cd-ce94a57e280e",
+            "id": "B00000002",
             "parents": [],
             "selfClosing": false,
             "type": "text",
           },
           {
             "attributes": {},
-            "id": "16a33c18-c408-4bfc-b129-c8e6e40bd083",
+            "id": "B00000003",
             "parents": [],
             "selfClosing": false,
             "type": "blockquote",
           },
           {
             "attributes": {},
-            "id": "5bbdfba0-02a4-413f-88d8-66ec4242d4c8",
+            "id": "B00000004",
             "parents": [],
             "selfClosing": false,
             "type": "text",
           },
           {
             "attributes": {},
-            "id": "9521c971-2718-4e98-9738-21c779e01ae6",
+            "id": "B00000005",
             "parents": [],
             "selfClosing": false,
             "type": "paragraph",
